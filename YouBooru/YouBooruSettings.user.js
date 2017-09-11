17879756982831477b9e38b4f52a1636fca378cd
stsyn <stasyan1902@gmail.com>
2017-09-11 15:26:36 +0500

    Update YouBooruSettings.user.js
    - "share script" feature

--- YouBooru/YouBooruSettings.user.js
+++ YouBooru/YouBooruSettings.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         YourBooru:Settings
 // @namespace    http://tampermonkey.net/
+
 // @include      *://trixiebooru.org/settings
 // @include      *://derpibooru.org/settings
 // @include      *://www.trixiebooru.org/settings
@@ -9,8 +10,18 @@
 // @include      *://*.orzgs6djmvrg633souxg64th.*.*/settings
 // @include      *://*.o53xo.mrsxe4djmjxw64tvfzxxezy.*.*/settings
 // @include      *://*.mrsxe4djmjxw64tvfzxxezy.*.*/settings
+
+// @include      *://trixiebooru.org/pages/yourbooru*
+// @include      *://derpibooru.org/pages/yourbooru*
+// @include      *://www.trixiebooru.org/pages/yourbooru*
+// @include      *://www.derpibooru.org/pages/yourbooru*
+// @include      *://*.o53xo.orzgs6djmvrg633souxg64th.*.*/pages/yourbooru*
+// @include      *://*.orzgs6djmvrg633souxg64th.*.*/pages/yourbooru*
+// @include      *://*.o53xo.mrsxe4djmjxw64tvfzxxezy.*.*/pages/yourbooru*
+// @include      *://*.mrsxe4djmjxw64tvfzxxezy.*.*/pages/yourbooru*
+
 // @downloadURL  https://github.com/stsyn/derpibooruscripts/raw/master/YouBooru/YouBooruSettings.user.js
-// @version      0.3
+// @version      0.3.1
 // @description  Feedz
 // @author       stsyn
 // @grant        none
@@ -83,7 +94,7 @@
 		x = document.createElement('input');
 		x.type = 'text';
 		x.value = f.name;
-		x.style.width = '15em';
+		x.style.width = '18.3em';
 		x.id = i;
 		x.className = 'input';
 		x.addEventListener('input', function() {feedz[this.id].name = this.value; write();});
@@ -119,7 +130,7 @@
 
 		x = document.createElement('span');
 		x.style.marginLeft = '.5em';
-		x.innerHTML = 'Cache (in minutes)';
+		x.innerHTML = 'Cache (minutes)';
 		l.appendChild(x);
 
 		x = document.createElement('input');
@@ -132,7 +143,7 @@
 		l.appendChild(x);
 
 		x = document.createElement('span');
-		x.innerHTML = ' ... or update each (in minutes)';
+		x.innerHTML = ' ... or update each';
 		l.appendChild(x);
 
 		x = document.createElement('input');
@@ -142,6 +153,15 @@
 		x.style.width = '4em';
 		x.className = 'input';
 		x.addEventListener('input', function() {feedz[this.id].ccache = parseInt(this.ccache); write();});
+		l.appendChild(x);
+
+		x = document.createElement('a');
+		x.style.marginLeft = '.5em';
+		x.classList = 'button';
+		x.innerHTML = 'Share';
+		x.target = '_blank';
+		x.title = 'Copy this link and paste it somewhere to share that feed!';
+		x.href = '/pages/yourbooru?addFeed?'+f.name+'?'+encodeURIComponent(f.query).replace(/\(/,'%28').replace(/\)/,'%29')+'?'+f.sort+'?'+f.sd+'?'+f.cache+'?'+f.ccache;
 		l.appendChild(x);
 
 		if (i > 0) {
@@ -239,191 +259,250 @@
 		e.appendChild(l);
 	}
 
-	register();
-	let cont = document.createElement('div');
-	cont.className = 'block__tab hidden';
-	cont.setAttribute('data-tab','YourBooru');
-	document.getElementById('js-setting-table').insertBefore(cont, document.querySelector('[data-tab="local"]').nextSibling);
+	function settingPage() {
 
-	let tab = document.createElement('a');
-	tab.href = '#';
-	tab.setAttribute('data-click-tab','YourBooru');
-	tab.innerHTML = 'YourBooru';
-	document.getElementsByClassName('block__header')[0].appendChild(tab);
+		let cont = document.createElement('div');
+		cont.className = 'block__tab hidden';
+		cont.setAttribute('data-tab','YourBooru');
+		document.getElementById('js-setting-table').insertBefore(cont, document.querySelector('[data-tab="local"]').nextSibling);
 
-	let el;
-	let ax = JSON.parse(localStorage._ydb_main);
+		let tab = document.createElement('a');
+		tab.href = '#';
+		tab.setAttribute('data-click-tab','YourBooru');
+		tab.innerHTML = 'YourBooru';
+		document.getElementsByClassName('block__header')[0].appendChild(tab);
 
-	el = document.createElement('div');
-	el.className = 'block block--fixed block--warning';
-	el.innerHTML = 'Settings on this tab are saved in the current browser. They are independent of whether you are logged in or not.<br>Every setting will apply when you change anything.';
-	cont.appendChild(el);
-	if (ax.feeds != undefined) {
-		el = document.createElement('h4');
-		el.innerHTML = 'Feeds';
+		let el;
+		let ax = JSON.parse(localStorage._ydb_main);
+
+		el = document.createElement('div');
+		el.className = 'block block--fixed block--warning';
+		el.innerHTML = 'Settings on this tab are saved in the current browser. They are independent of whether you are logged in or not.<br>Every setting will apply when you change anything.';
 		cont.appendChild(el);
+		if (ax.feeds != undefined) {
+			el = document.createElement('h4');
+			el.innerHTML = 'Feeds';
+			cont.appendChild(el);
 
-		let el2 = document.createElement('label');
-		el2.innerHTML = 'Do not remove watchlist ';
-		cont.appendChild(el2);
-		el = document.createElement('input');
-		el.type = 'checkbox';
-		el.checked = config.feeds.doNotRemoveWatchList;
-		el.addEventListener('change', function() {config.feeds.doNotRemoveWatchList = this.checked; writeConfig();});
-		el2.appendChild(el);
+			let el2 = document.createElement('label');
+			el2.innerHTML = 'Do not remove watchlist ';
+			cont.appendChild(el2);
+			el = document.createElement('input');
+			el.type = 'checkbox';
+			el.checked = config.feeds.doNotRemoveWatchList;
+			el.addEventListener('change', function() {config.feeds.doNotRemoveWatchList = this.checked; writeConfig();});
+			el2.appendChild(el);
 
-		el2 = document.createElement('p');
-		el2.innerHTML = 'Images in each feed: ';
-		cont.appendChild(el2);
-		el = document.createElement('input');
-		el.type = 'text';
-		el.value = config.feeds.imagesInFeeds;
-		el.style.width = '3em';
-		el.className = 'input';
-		el.addEventListener('input', function() {config.feeds.imagesInFeeds = parseInt(this.value); writeConfig();});
-		el2.appendChild(el);
+			el2 = document.createElement('p');
+			el2.innerHTML = 'Images in each feed: ';
+			cont.appendChild(el2);
+			el = document.createElement('input');
+			el.type = 'text';
+			el.value = config.feeds.imagesInFeeds;
+			el.style.width = '3em';
+			el.className = 'input';
+			el.addEventListener('input', function() {config.feeds.imagesInFeeds = parseInt(this.value); writeConfig();});
+			el2.appendChild(el);
 
-		el2 = document.createElement('label');
-		el2.innerHTML = 'Watch link on the right side ';
-		cont.appendChild(el2);
-		el = document.createElement('input');
-		el.type = 'checkbox';
-		el.checked = config.feeds.watchFeedLinkOnRightSide;
-		el.addEventListener('change', function() {config.feeds.watchFeedLinkOnRightSide = this.checked; writeConfig();});
-		el2.appendChild(el);
+			el2 = document.createElement('label');
+			el2.innerHTML = 'Watch link on the right side ';
+			cont.appendChild(el2);
+			el = document.createElement('input');
+			el.type = 'checkbox';
+			el.checked = config.feeds.watchFeedLinkOnRightSide;
+			el.addEventListener('change', function() {config.feeds.watchFeedLinkOnRightSide = this.checked; writeConfig();});
+			el2.appendChild(el);
 
-		el2 = document.createElement('label');
-		el2.innerHTML = '<br>Remove unneeded content from HTML (disable if issues happens (but it will fill up your localStorage!))';
-		cont.appendChild(el2);
-		el = document.createElement('input');
-		el.type = 'checkbox';
-		el.checked = config.feeds.optimizeLS;
-		el.addEventListener('change', function() {config.feeds.optimizeLS = this.checked; writeConfig();});
-		el2.appendChild(el);
+			el2 = document.createElement('label');
+			el2.innerHTML = '<br>Remove unneeded content from HTML (disable if issues happens (but it will fill up your localStorage!))';
+			cont.appendChild(el2);
+			el = document.createElement('input');
+			el.type = 'checkbox';
+			el.checked = config.feeds.optimizeLS;
+			el.addEventListener('change', function() {config.feeds.optimizeLS = this.checked; writeConfig();});
+			el2.appendChild(el);
 
-		let fel = document.createElement('div');
-		cont.appendChild(fel);
+			let fel = document.createElement('div');
+			cont.appendChild(fel);
 
-		el2 = document.createElement('span');
-		el2.classList = 'button';
-		el2.innerHTML = 'Add feed';
-		el2.addEventListener('click',function() {
-			if (feedz != undefined) for (let i=0; i<=feedz.length; i++) {
-				if (feedz[i] == undefined) {
-					feedz[i] = {
-						name:'New feed',
-						query:'*',
-						sort:'',
-						sd:'desc',
-						cache:30,
-						double:false
-					};
-					write();
-					break;
+			el2 = document.createElement('span');
+			el2.classList = 'button';
+			el2.innerHTML = 'Add feed';
+			el2.addEventListener('click',function() {
+				if (feedz != undefined) for (let i=0; i<=feedz.length; i++) {
+					if (feedz[i] == undefined) {
+						feedz[i] = {
+							name:'New feed',
+							query:'*',
+							sort:'',
+							sd:'desc',
+							cache:30,
+							double:false
+						};
+						write();
+						break;
+					}
 				}
-			}
-			location.reload();
-		});
-		cont.appendChild(el2);
-
-		el2 = document.createElement('span');
-		el2.classList = 'button commission__category';
-		el2.innerHTML = 'Reset feeds cache';
-		el2.style.marginLeft = '1em';
-		el2.style.marginRight = '6em';
-		el2.addEventListener('click',resetCaches);
-		cont.appendChild(el2);
-
-		el2 = document.createElement('input');
-		el2.type = 'checkbox';
-		el2.id = 'ydb_reset';
-		cont.appendChild(el2);
-
-		el2 = document.createElement('span');
-		el2.classList = 'button commission__category';
-		el2.innerHTML = 'Reset everything';
-		el2.style.marginLeft = '.5em';
-		el2.style.marginRight = '6em';
-		el2.addEventListener('click', function() {
-			if (document.getElementById('ydb_reset').checked) {
-				feedz = undefined;
-				write();
-				location.href = '/';
-			}
-			else {
-				alert('Mark checkbox first!');
-			}
-		});
-		cont.appendChild(el2);
-
-		el2 = document.createElement('input');
-		el2.type = 'checkbox';
-		el2.id = 'ydb_feeds_del';
-		cont.appendChild(el2);
-
-		el2 = document.createElement('span');
-		el2.classList = 'button commission__category';
-		el2.innerHTML = 'I uninstalled YourBooru: Feeds.<br>';
-		el2.style.marginLeft = '.5em';
-		el2.href = '#';
-		el2.addEventListener('click', function() {
-			if (document.getElementById('ydb_feeds_del').checked) {
-				feedz = undefined;
-				write();
-				ax.feeds = undefined;
-				localStorage._ydb_main = JSON.stringify(ax);
 				location.reload();
+			});
+			cont.appendChild(el2);
+
+			el2 = document.createElement('span');
+			el2.classList = 'button commission__category';
+			el2.innerHTML = 'Reset feeds cache';
+			el2.style.marginLeft = '1em';
+			el2.style.marginRight = '6em';
+			el2.addEventListener('click',resetCaches);
+			cont.appendChild(el2);
+
+			el2 = document.createElement('input');
+			el2.type = 'checkbox';
+			el2.id = 'ydb_reset';
+			cont.appendChild(el2);
+
+			el2 = document.createElement('span');
+			el2.classList = 'button commission__category';
+			el2.innerHTML = 'Reset everything';
+			el2.style.marginLeft = '.5em';
+			el2.style.marginRight = '6em';
+			el2.addEventListener('click', function() {
+				if (document.getElementById('ydb_reset').checked) {
+					feedz = undefined;
+					write();
+					location.href = '/';
+				}
+				else {
+					alert('Mark checkbox first!');
+				}
+			});
+			cont.appendChild(el2);
+
+			el2 = document.createElement('input');
+			el2.type = 'checkbox';
+			el2.id = 'ydb_feeds_del';
+			cont.appendChild(el2);
+
+			el2 = document.createElement('span');
+			el2.classList = 'button commission__category';
+			el2.innerHTML = 'I uninstalled YourBooru: Feeds.<br>';
+			el2.style.marginLeft = '.5em';
+			el2.href = '#';
+			el2.addEventListener('click', function() {
+				if (document.getElementById('ydb_feeds_del').checked) {
+					feedz = undefined;
+					write();
+					ax.feeds = undefined;
+					localStorage._ydb_main = JSON.stringify(ax);
+					location.reload();
+				}
+				else {
+					alert('Mark checkbox first!');
+				}
+			});
+			cont.appendChild(el2);
+
+			if (feedz != undefined) {
+				for (let i=0; i<feedz.length; i++) if (feedz[i] == undefined) {
+					feedz.splice(i,1);
+					i--;
+				};
+				for (let i=0; i<feedz.length; i++) putLine(fel, feedz[i], i);
 			}
 			else {
-				alert('Mark checkbox first!');
+				fel.classList.add('block--warning');
+				fel.classList.add('block');
+				fel.classList.add('block--fixed');
+				fel.innerHTML = 'Everything is lost! Please visit derpibooru mainpage to reload default feeds and make everything work again!';
 			}
-		});
-		cont.appendChild(el2);
-
-		if (feedz != undefined) {
-			for (let i=0; i<feedz.length; i++) if (feedz[i] == undefined) {
-				feedz.splice(i,1);
-				i--;
-			};
-			for (let i=0; i<feedz.length; i++) putLine(fel, feedz[i], i);
 		}
+		if (ax.settings != undefined) {
+			el = document.createElement('h4');
+			el.innerHTML = '&nbsp';
+			cont.appendChild(el);
+			el = document.createElement('h4');
+			el.innerHTML = 'Installed plugins';
+			cont.appendChild(el);
+			let s = '';
+			el = document.createElement('p');
+			for (let key in ax) {
+				if (key == 'feeds') s+='YourBooru: Feeds ';
+				else if (key == 'settings') s+='YourBooru: Settings ';
+				else s+='id='+key+' ';
+
+				s+='ver. '+ax[key].version;
+				s+='<br>';
+			}
+			el.innerHTML =s;
+			cont.appendChild(el);
+		}
+		let hh = function () {
+			if (location.hash!='') {
+				let t = location.hash.slice(1);
+				if (document.querySelector('a[data-click-tab="'+t+'"]') != null) document.querySelector('a[data-click-tab="'+t+'"]').click();
+			}
+		};
+
+		for (let i=0; i<document.getElementsByClassName('block__header')[0].childNodes.length; i++) {
+			document.getElementsByClassName('block__header')[0].childNodes[i].href = '#'+document.getElementsByClassName('block__header')[0].childNodes[i].getAttribute('data-click-tab');
+			document.getElementsByClassName('block__header')[0].childNodes[i].addEventListener('click', function(e) {location.hash = e.target.href.split('#')[1];});
+		}
+		setTimeout(hh, 500);
+	}
+
+	////////////////////////////
+
+	function YB_createEmpty() {
+		document.querySelector('#content h1').innerHTML = 'Ooops!';
+		document.querySelector('#content .walloftext').innerHTML = '<p>YourBooru cannot get what you want. Sorry :(</p><p>Try to check url you used. If you believe that you found a bug report to me please!</p>';
+	}
+
+	function YB_addFeed(u) {
+		document.querySelector('#content h1').innerHTML = 'Add feed (feature in beta!)';
+		var c = document.querySelector('#content .walloftext');
+		if (u.length>6) {
+			c.innerHTML = 'Name: '+decodeURIComponent(u[1]);
+			c.innerHTML += '<br>Query: <a href="/search?q='+u[2]+'&sf='+u[3]+'&sd='+u[4]+'">'+decodeURIComponent(u[2]).replace(/\\+/g,' ')+'</a>';
+			c.innerHTML += '<br>Sorting type: '+u[3];
+			c.innerHTML += '<br>Sorting direction: '+u[4];
+			c.innerHTML += '<br>Update interval: '+(u[5]==0?'each'+u[6]:u[5])+' minutes';
+			c.innerHTML += '<br><br><span id="yy_add" class="button">Add feed</span> <span id="yy_cancel" class="button">Cancel</span>';
+
+			document.getElementById('yy_cancel').addEventListener('click', function() {
+				if (history.length == 1) close();
+				else history.back();
+			});
+
+			document.getElementById('yy_add').addEventListener('click', function() {
+				var f = {};
+				f.loaded = false;
+				f.name = decodeURIComponent(u[1]);
+				f.query = decodeURIComponent(u[2]).replace(/\\+/g,' ');
+				f.sort = u[3];
+				f.sd = u[4];
+				f.cache = u[5];
+				f.ccache = u[6];
+				feedz.push(f);
+                write();
+
+				if (history.length == 1) close();
+				else history.back();
+			});
+		}
+		else c.innerHTML = 'Not enough parameters, impossible to add!';
+	}
+
+	function yourbooruPage() {
+		let x = location.search.slice(1);
+		if (location.search == "") YB_createEmpty();
+		else if (location.search == "?") YB_createEmpty();
 		else {
-			fel.classList.add('block--warning');
-			fel.classList.add('block');
-			fel.classList.add('block--fixed');
-			fel.innerHTML = 'Everything is lost! Please visit derpibooru mainpage to reload default feeds and make everything work again!';
+			let u = x.split('?');
+			if (u[0] == "addFeed") YB_addFeed(u);
+			else YB_createEmpty();
 		}
 	}
-	if (ax.settings != undefined) {
-		el = document.createElement('h4');
-		el.innerHTML = '&nbsp';
-		cont.appendChild(el);
-		el = document.createElement('h4');
-		el.innerHTML = 'Installed plugins';
-		cont.appendChild(el);
-		let s = '';
-		el = document.createElement('p');
-		for (let key in ax) {
-			if (key == 'feeds') s+='YourBooru: Feeds ';
-			else if (key == 'settings') s+='YourBooru: Settings ';
-			else s+='id='+key+' ';
 
-			s+='ver. '+ax[key].version;
-			s+='<br>';
-		}
-		el.innerHTML =s;
-		cont.appendChild(el);
-	}
-	let hh = function () {
-		if (location.hash!='') {
-			let t = location.hash.slice(1);
-			if (document.querySelector('a[data-click-tab="'+t+'"]') != null) document.querySelector('a[data-click-tab="'+t+'"]').click();
-		}
-	};
-
-	for (let i=0; i<document.getElementsByClassName('block__header')[0].childNodes.length; i++) {
-		document.getElementsByClassName('block__header')[0].childNodes[i].href = '#'+document.getElementsByClassName('block__header')[0].childNodes[i].getAttribute('data-click-tab');
-		document.getElementsByClassName('block__header')[0].childNodes[i].addEventListener('click', function(e) {location.hash = e.target.href.split('#')[1];});
-	}
-	setTimeout(hh, 500);
+	if (location.pathname == "/settings") settingPage();
+	if (location.pathname == "/pages/yourbooru") yourbooruPage();
+	register();
 })();
