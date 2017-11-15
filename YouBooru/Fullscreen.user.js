419cf5cd31929135bb8a97b5b8237e3058e55a3f
stsyn <stasyan1902@gmail.com>
2017-11-15 20:53:53 +0500

    Update Fullscreen.user.js

--- YouBooru/Fullscreen.user.js
+++ YouBooru/Fullscreen.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Resurrected Derp Fullscreen
 // @namespace    https://github.com/stsyn/derp-fullscreen/
-// @version      0.3
+// @version      0.3.1
 // @description  Make Fullscreen great again!
 // @author       St@SyaN
 // @include      *://trixiebooru.org/*
@@ -21,7 +21,7 @@
     'use strict';
     var styles = {};
     styles.general = `
-.header, #content>.block:first-child, #footer, h4 {
+.header,#content>.block:first-child, #footer, h4 {
 height:0;
 overflow-y:hidden;
 margin:0;
@@ -30,6 +30,25 @@ padding:0;
 #content {
 margin:0;
 padding:0;
+}
+.block__header a.js-rand,
+.block__header a.js-up,
+.block__header a.js-prev i,
+.block__header a.js-next i{
+display:none;
+}
+.block__header a.js-prev,
+.block__header a.js-next{
+position:fixed;
+top:0;
+height:100%;
+width:25%;
+}
+.block__header a.js-prev {
+left:0;
+}
+.block__header a.js-next {
+right:0;
 }
 .image-show-container {
 position:absolute;
@@ -59,11 +78,94 @@ max-width:none;
 margin:auto;
 display:block;
 }
+#_ydb_fs_disable {
+left:0;
+position:absolute;
+z-index:202;
+display:inline-block !important;
+opacity:0.3;
+}
+`;
+    styles.base = `
+#content>.block:first-child{
+position:fixed;
+top:0;
+height:auto;
+width:100%;
+z-index:201;
+}
+
+#content>.block:first-child>.block__header--sub {
+float:right;
+width:auto;
+opacity:0.2;
+}
+
+#content>.block:first-child>.block__header,
+#content>.block:first-child>.block__header a:not(.active){
+background:none;
+}
+
+#content>.block:first-child>.block__header {
+font-size:150%;
+line-height:175%;
+}
+
+#content>.block:first-child>.block__header>.block__header__title {
+font-size:100%;
+line-height:100%;
+transition-duration: 0.1s;
+transition-timing-function: ease-in-out;
+}
+
+#content>.block:first-child>.block__header>.block__header__title:hover {
+color:#000;
+transition-duration: 0.1s;
+transition-timing-function: ease-in-out;
+}
+
+#content>.block:first-child>.block__header--sub .image_uploader,
+#content>.block:first-child>.block__header--sub time,
+#content>.block:first-child>.block__header [class*="js-notification-Image"],
+#content>.block:first-child>.block__header [href*="/related"],
+#content>.block:first-child>.block__header [href*="/view/"],
+#content>.block:first-child>.block__header [title*="with tags"],
+#content>.block:first-child>.block__header .block__header__dropdown-tab {
+display:none;
+}
+
+#content>.block:first-child>.block__header [href*="/download/"] {
+right:0;
+position:absolute;
+z-index:202;
+opacity:0.3;
+}
+
+.block__header .stretched-mobile-links {
+width:0;
+}
+
 `;
 
     var popUps = {};
     var objects = {};
     var pub = {};
+    var settings;
+
+
+    function write() {
+        localStorage._ydb_fs = JSON.stringify(settings);
+    }
+
+    var svd = localStorage._ydb_fs;
+    try {
+        settings = JSON.parse(svd);
+    }
+    catch (e) {
+        settings = {};
+        settings.enabled = false;
+        write();
+    }
 
     /*function register() {
         console.log(window._YDB_public);
@@ -76,22 +178,37 @@ display:block;
         };
     }*/
 
+
     function addElem(tag, values, parent) {
         if (values != undefined && values.id != undefined && document.getElementById(values.id) != undefined) return document.getElementById(values.id);
         var t = document.createElement(tag);
-        for (var i in values) t[i] = values[i];
+        for (var i in values) if (i!='events') t[i] = values[i];
+        try {
+            if (values.events != undefined) values.events.forEach(function(v,i,a) {
+                t.addEventListener(v.t, v.f);
+            }
+        );}
+        catch (e) {
+        }
         parent.appendChild(t);
         return t;
     }
 
-    function append(st) {
-        if (document.getElementById(st) != undefined) return 1;
-        addElem('style', {innerHTML:st, id:st}, document.head);
+    function append(id) {
+        if (document.getElementById(id) != undefined) return 1;
+        addElem('style', {innerHTML:styles[id], id:id}, document.head);
+        return 0;
+    }
+
+    function remove(id) {
+        if (document.getElementById(id) == undefined) return 1;
+        document.head.removeChild(document.getElementById(id));
         return 0;
     }
 
     function preenable() {
-        append(styles.general);
+        append('general');
+        append('base');
     }
 
     function rescale() {
@@ -113,14 +230,25 @@ display:block;
         }
     }
 
-    function enable() {
+    function init() {
+        objects.disable = addElem('a', {id:'_ydb_fs_disable', className:'', style:'display:none', innerHTML:'Disable', events:[{t:'click',f:function() {disable();}}]}, document.querySelector('#content>.block:first-child>.block__header'));
+        objects.enable = addElem('a', {id:'_ydb_fs_enable', className:'header__link', innerHTML:'Fullscreen', events:[{t:'click',f:function() {enable(true);}}]}, document.body);
+        document.getElementsByClassName('flex-row__right')[0].insertBefore(objects.enable, document.getElementsByClassName('flex-row__right')[0].childNodes[0]);
+    }
+
+    function enable(notInital) {
+        if (notInital) preenable();
+        init();
+
         popUps.back = addElem('div', {id:'_ydb_fs_backSide', className:'_fs_popup_back'}, document.body);
         popUps.coms = addElem('div', {id:'_ydb_fs_commPopup', className:'_fs_popup _fs_popup_big'}, document.body);
         popUps.down = addElem('div', {id:'_fs_down', className:'_fs_down'}, document.body);
+
         document.querySelectorAll('#content>.layout--narrow')[1].appendChild(document.getElementById('image_options_area'));
         popUps.coms.appendChild(document.querySelectorAll('#content>.layout--narrow')[2]);
         popUps.down.appendChild(document.querySelectorAll('#content>.layout--narrow')[0]);
         popUps.down.appendChild(document.querySelectorAll('#content>.layout--narrow')[0]);
+
         objects.image = document.getElementById('image-display');
         objects.icontainer = document.getElementsByClassName('image-show-container')[0];
         objects.dcontainer = document.getElementById('image_target');
@@ -135,11 +263,35 @@ display:block;
             pub.loaded = true;
             rescale();
         }
+        pub.enabled = true;
+        settings.enabled = true;
+
+        write();
+    }
+
+    function disable() {
+        remove('general');
+        remove('base');
+
+        document.getElementById('content').appendChild(popUps.down.querySelectorAll('.layout--narrow')[0]);
+        document.getElementById('content').appendChild(popUps.down.querySelectorAll('.layout--narrow')[0]);
+        document.getElementById('content').appendChild(popUps.coms.querySelectorAll('.layout--narrow')[0]);
+        document.querySelectorAll('#content>.layout--narrow')[2].insertBefore(document.getElementById('image_options_area'), document.querySelectorAll('#content>.layout--narrow')[2].childNodes[0]);
+        pub.enabled = false;
+        settings.enabled = false;
+
+        write();
     }
 
     if (parseInt(location.pathname.slice(1))>=0 || (location.pathname.split('/')[1] == 'images' && parseInt(location.pathname.split('/')[2])>=0)) {
-        preenable();
-        if (document.readyState !== 'loading') enable();
-        else document.addEventListener('DOMContentLoaded', enable);
+        if (settings.enabled) {
+            preenable();
+            if (document.readyState !== 'loading') enable();
+            else document.addEventListener('DOMContentLoaded', enable);
+        }
+        else {
+            if (document.readyState !== 'loading') init();
+            else document.addEventListener('DOMContentLoaded', init);
+        }
     }
 }());
