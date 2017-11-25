3fc0058ea68253fd3b78755cf19376776590a9ce
stsyn <stasyan1902@gmail.com>
2017-11-25 21:13:27 +0500

    Add files via upload

--- YouBooru/lib.js
+++ YouBooru/lib.js
@@ -11,6 +11,37 @@ function addElem(tag, values, parent) {
 	return t;
 }
 
+function ChildsAddElem(tag, values,parent, childs) {
+	var t;
+	if (values != undefined && values.id != undefined && document.getElementById(values.id) != undefined) {
+		if (document.querySelectorAll(tag+'#'+values.id).length == 0) {
+			t = document.getElementById(values.id);
+			t.parentNode.removeChild(t);
+			t = document.createElement(tag);
+		}
+		else {
+			t = document.getElementById(values.id);
+			while (t.firstChild) {
+			  t.removeChild(t.firstChild);
+			}
+		}
+	}
+	else t = document.createElement(tag);
+	
+	for (var i in values) if (i!='events' && i!='dataset') t[i] = values[i];
+	if (values.dataset != undefined) for (var i in values.dataset) t.dataset[i] = values.dataset[i];
+	if (values.events != undefined) values.events.forEach(function(v,i,a) {
+		t.addEventListener(v.t, v.f);
+	});
+	
+	if (childs != undefined && childs.length != undefined) childs.forEach(function(c,i,a) {
+		t.appendChild(c);
+	});
+	
+	parent.appendChild(t);
+	return t;
+}
+
 function InfernoAddElem(tag, values, childs) {
 	var t;
 	if (values != undefined && values.id != undefined && document.getElementById(values.id) != undefined) {
