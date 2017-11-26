6fdb60d77a209519901c1feab57516295b12ff5a
stsyn <stasyan1902@gmail.com>
2017-11-26 15:07:58 +0500

    Update Fullscreen.user.js

--- YouBooru/Fullscreen.user.js
+++ YouBooru/Fullscreen.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Resurrected Derp Fullscreen
 // @namespace    https://github.com/stsyn/derp-fullscreen/
-// @version      0.3.4
+// @version      0.3.6
 // @description  Make Fullscreen great again!
 // @author       St@SyaN
 
@@ -35,7 +35,18 @@
     'use strict';
     var styles = {};
     styles.general = `
-.header,#content>.block:first-child, #footer, h4, .flash--site-notice {
+#_ydb_fs_mainPopup {
+position:relative
+}
+footer {
+position:absolute;
+bottom:0;
+width:calc(100% - 12px);
+}
+._fs #footer {
+background:none
+}
+#content>.block:first-child, h4, .flash--site-notice, ._fs #_ydb_fs_enable {
 height:0;
 overflow-y:hidden;
 margin:0;
@@ -53,8 +64,8 @@ margin:0;
 padding:0;
 padding-left:0 !important;
 }
-.block__header a.js-rand,
-.block__header a.js-up,
+#content>.block:first-child>.block__header a.js-rand,
+#content>.block:first-child>.block__header a.js-up,
 #content>.block:first-child>.block__header a.js-prev i,
 #content>.block:first-child>.block__header a.js-next i{
 display:none;
@@ -95,10 +106,15 @@ background:rgba(0,0,0,.5);
 ._fs_popup {
 display:none;
 margin:auto;
-overflow-y:scroll;
+overflow-y:auto;
 }
 ._fs_popup_big {
 max-width:1000px;
+height:80vh;
+margin-top:10vh;
+}
+._fs_popup_vbig {
+width:95vw;
 height:80vh;
 margin-top:10vh;
 }
@@ -167,7 +183,8 @@ transition-timing-function: ease-in-out;
 color:#e0e0e0;
 }
 
-#content>.block:first-child>.block__header>.interaction--comments:hover {
+#content>.block:first-child>.block__header>.interaction--comments:hover,
+#_fs_main:hover{
 color:#e0e0e0;
 }
 
@@ -239,7 +256,7 @@ height:60vh;
 ._fs_down.active ._fs_down_container{
 margin-top:1vh;
 height:59vh;
-overflow-y:scroll;
+overflow-y:auto;
 }
 ._fs_down.active ._fs_down_back{
 height:100%;
@@ -378,16 +395,35 @@ transition:.5s;
         setTimeout(listener, 100);
 
         tagEditor();
-        if (pub.mouseY/window.innerHeight >= (popUps.down.classList.contains('active')?0.4:0.85)) popUps.down.classList.add('active');
+        if (pub.mouseY/window.innerHeight >= (popUps.down.classList.contains('active')?0.4:0.85) && !popUps.back.classList.contains('active')) popUps.down.classList.add('active');
         else popUps.down.classList.remove('active');
 
-        if ((pub.mouseY/window.innerHeight > .15 && pub.mouseY/window.innerHeight < .85 && pub.mouseX/window.innerWidth > .15 && pub.mouseX/window.innerWidth < .85) &&
+        let s = '';
+        if (parseInt(document.getElementsByClassName('js-notification-ticker')[0].innerHTML) > 0) s += ' '+parseInt(document.getElementsByClassName('js-notification-ticker')[0].innerHTML);
+        if (parseInt(document.querySelector('a[href="/messages"] .fa-embedded__text').innerHTML) > 0) s += (s.length>0?'+':'')+'M'+parseInt(document.querySelector('a[href="/messages"] .fa-embedded__text').innerHTML);
+
+        if (s.length>0) {
+            objects.mainButtonNotify.innerHTML = s;
+            objects.mainButton.style.background = '#800';
+        }
+        else {
+            objects.mainButtonNotify.innerHTML = '';
+            objects.mainButton.style.background = '';
+        }
+
+        if ((pub.mouseY/window.innerHeight > 0.15 && pub.mouseY/window.innerHeight < 0.85) &&
            !(popUps.down.classList.contains('active') || popUps.back.classList.contains('active')))  {
             pub.static++;
-            if (pub.static > 20) document.body.classList.add('_ydb_fs_static');
+            if (pub.static > 15 && !document.body.classList.contains('_ydb_fs_static')) {
+                settings.hidden = true;
+                write();
+                document.body.classList.add('_ydb_fs_static');
+            }
         }
         else {
             pub.static =0;
+            settings.hidden = false;
+            write();
             document.body.classList.remove('_ydb_fs_static');
         }
     }
@@ -401,16 +437,58 @@ transition:.5s;
         if (notInital) preenable();
         init();
 
+        pub.mouseX = window.innerWidth/2;
+        pub.mouseY = window.innerHeight/2;
+        if (settings.hidden) pub.static = 100;
+        else pub.static = 0;
+
         popUps.back = addElem('div', {id:'_ydb_fs_backSide', className:'_fs_popup_back'}, document.body);
         popUps.coms = addElem('div', {id:'_ydb_fs_commPopup', className:'_fs_popup _fs_popup_big block__content'}, popUps.back);
+        popUps.main = addElem('div', {id:'_ydb_fs_mainPopup', className:'_fs_popup _fs_popup_vbig block__content'}, popUps.back);
         popUps.down = addElem('div', {id:'_fs_down', className:'_fs_down'}, document.body);
         popUps.downBack = addElem('div', {id:'_fs_down_back', className:'_fs_down_back tag', dataset:{tagCategory:'rating'}}, popUps.down);
         popUps.downContainer = addElem('div', {id:'_fs_down_container', className:'_fs_down_container'}, popUps.down);
+        objects.mainButton = InfernoAddElem('a', {id:'_fs_main', events:[{
+            t:'click',
+            f:function() {
+                callPopup('main');
+            }
+        }]}, [
+            InfernoAddElem('span', {}, [
+                InfernoAddElem('i', {className:'fa', innerHTML:'\uf0c9'}, []),
+                objects.mainButtonNotify = InfernoAddElem('span', {}, [])
+            ])
+        ]);
 
+        document.querySelector('#content>.block:first-child>.block__header').insertBefore(objects.mainButton,document.getElementsByClassName('interaction--fave')[0]);
         document.querySelectorAll('#content>.layout--narrow')[1].appendChild(document.getElementById('image_options_area'));
         popUps.coms.appendChild(document.querySelectorAll('#content>div')[4]);
         popUps.downContainer.appendChild(document.querySelectorAll('#content>div')[2]);
         popUps.downContainer.appendChild(document.querySelectorAll('#content>div')[2]);
+
+        popUps.main.appendChild(document.querySelector('header'));
+        popUps.main.appendChild(document.querySelector('nav.header.header--secondary'));
+        ChildsAddElem('div', {className:'block__header'}, popUps.main, [
+            InfernoAddElem('span',{innerHTML:'Image Tools:',style:'margin-left:1em'}, []),
+            InfernoAddElem('a',{className:'js-up', events:[{t:'click',f:function() {
+                document.querySelector('#content>.block:first-child>.block__header .js-up').click();
+            }}]}, [
+                InfernoAddElem('i',{className:'fa fa-chevron-up'},[]),
+                InfernoAddElem('span',{innerHTML:' Find'},[])
+            ]),
+            InfernoAddElem('a',{className:'js-random', events:[{t:'click',f:function() {
+                document.querySelector('#content>.block:first-child>.block__header .js-random').click();
+            }}]}, [
+                InfernoAddElem('i',{className:'fa fa-random'},[]),
+                InfernoAddElem('span',{innerHTML:' Random'},[])
+            ]),
+            document.querySelectorAll('#content>.block:first-child>.block__header [class*="js-notification"')[0].cloneNode(true),
+            document.querySelectorAll('#content>.block:first-child>.block__header [class*="js-notification"')[1].cloneNode(true),
+            document.querySelector('#content>.block:first-child>.block__header .dropdown').cloneNode(true),
+            document.querySelector('#content>.block:first-child>.block__header [href*="/related/"').cloneNode(true),
+            document.querySelector('#content>.block:first-child>.block__header>.hide-mobile').cloneNode(true)
+        ]);
+        popUps.main.appendChild(document.querySelector('footer'));
 
         objects.image = document.getElementById('image-display');
         objects.de = document.documentElement;
@@ -425,6 +503,8 @@ transition:.5s;
         if (objects.image != undefined) loadedImgFetch();
         objects.dcontainer.addEventListener("DOMNodeInserted",loadedImgFetch);
 
+        document.body.classList.add('_fs');
+
         pub.enabled = true;
         settings.enabled = true;
 
@@ -435,6 +515,8 @@ transition:.5s;
     function disable() {
         remove('general');
         remove('base');
+        remove('ex');
+        remove('hider');
         unscale();
 
         objects.dcontainer.removeEventListener("DOMNodeInserted",loadedImgFetch);
@@ -442,12 +524,23 @@ transition:.5s;
         popUps.back.removeEventListener('click',hidePopups);
         document.body.removeEventListener('mousemove',mouseListener);
 
+        document.querySelector('#content>.block:first-child>.block__header').removeChild(objects.mainButton);
+
         document.getElementById('content').appendChild(popUps.downContainer.getElementsByTagName('div')[0]);
         document.getElementById('content').appendChild(popUps.downContainer.getElementsByTagName('div')[0]);
         document.getElementById('content').appendChild(popUps.coms.getElementsByTagName('div')[0]);
+
+        document.getElementById('container').insertBefore(document.querySelector('nav.header.header--secondary'),document.getElementById('container').firstChild);
+        document.getElementById('container').insertBefore(document.querySelector('header'),document.getElementById('container').firstChild);
+        document.getElementById('container').appendChild(document.querySelector('footer'));
+
         document.querySelectorAll('#content>div')[4].insertBefore(document.getElementById('image_options_area'), document.querySelectorAll('#content>div')[4].childNodes[0]);
         pub.enabled = false;
         settings.enabled = false;
+
+        document.body.classList.remove('_fs');
+        document.body.removeChild(popUps.back);
+        document.body.removeChild(popUps.down);
 
         write();
     }
