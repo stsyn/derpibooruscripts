3a7fa6b527834160f458cd37461f3984fcfaeeab
stsyn <stasyan1902@gmail.com>
2018-02-23 10:17:19 +0500

    YDB:S 0.9.3
    - bg backup fix

--- YouBooru/YouBooruSettings.user.js
+++ YouBooru/YouBooruSettings.user.js
@@ -24,7 +24,7 @@
 // @require      https://github.com/LZMA-JS/LZMA-JS/raw/master/src/lzma_worker-min.js
 
 // @downloadURL  https://github.com/stsyn/derpibooruscripts/raw/master/YouBooru/YouBooruSettings.user.js
-// @version      0.9.2
+// @version      0.9.3
 // @description  Global settings script for YourBooru script family
 // @author       stsyn
 // @grant        none
@@ -430,11 +430,12 @@
 				x.querySelector('form.edit_user')
 			]);
 			document.body.removeChild(x);
-            s.getElementById('serve_hidpi').checked = localStorage.serve_hidpi;
-            s.getElementById('serve_webm').checked = localStorage.serve_webm;
-            s.getElementById('webm').checked = localStorage.webm;
-            s.getElementById('hide_uploader').checked = localStorage.hide_uploader;
-            s.getElementById('dont_track').checked = localStorage.dont_track;
+            console.log(s);
+            s.querySelector('#serve_hidpi').checked = localStorage.serve_hidpi;
+            s.querySelector('#serve_webm').checked = localStorage.serve_webm;
+            s.querySelector('#webm').checked = localStorage.webm;
+            s.querySelector('#hide_uploader').checked = localStorage.hide_uploader;
+            s.querySelector('#dont_track').checked = localStorage.dont_track;
 			process();
 		};
 
