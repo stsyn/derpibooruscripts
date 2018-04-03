53566347047c50014495ac53ee080d319c63d43f
stsyn <stasyan1902@gmail.com>
2018-04-03 09:30:39 +0500

    Update badgesDB0.js

--- YouBooru/libs/badgesDB0.js
+++ YouBooru/libs/badgesDB0.js
@@ -1,16 +1,24 @@
 function getBadgesImplications() {
-	return [
-		{'Hard Work':['Perfect Pony Plot Provider','Artist']},
-		{'Best Artist':['Artist']},
-		{'Da Magicks!':['Fine Arts','Artist']},
-		{'An Artist Who Rocks':['Artist']},
-		{'A Really Classy Artist':['An Artist Who Rocks','Artist']},
-		{'A Really Hyper Artist':['A Really Classy Artist','An Artist Who Rocks','Artist']},
-		{'Chaotic Little Trees':['A Really Hyper Artist','A Really Classy Artist','An Artist Who Rocks','Artist']},
-		{'Cool Crow':['Magnificent Metadata Maniac']},
-		{'Bronze Patron':['Happy Derpy']},
-		{'Silver Patron':['Happy Derpy']},
-		{'Gold Patron':['Happy Derpy']},
-		{'Platinum Patron':['Happy Derpy']}
-	];
+	return {
+		normal:[
+			{'Hard Work':['Perfect Pony Plot Provider','Artist']},
+			{'Best Artist':['Artist']},
+			{'Da Magicks!':['Fine Arts','Artist']},
+			{'An Artist Who Rocks':['Artist']},
+			{'A Really Classy Artist':['An Artist Who Rocks','Artist']},
+			{'A Really Hyper Artist':['A Really Classy Artist','An Artist Who Rocks','Artist']},
+			{'Chaotic Little Trees':['A Really Hyper Artist','A Really Classy Artist','An Artist Who Rocks','Artist']},
+			{'Cool Crow':['Magnificent Metadata Maniac']},
+			{'Bronze Patron':['Happy Derpy']},
+			{'Silver Patron':['Happy Derpy']},
+			{'Gold Patron':['Happy Derpy']},
+			{'Platinum Patron':['Happy Derpy']}
+		],
+		extreme:[
+			{'Friendship, Art, and Magic (6 Years)':['Friendship, Art, and Magic (5 Years)']},
+			{'Helpful Owl':['Friendly Griffon']},
+			{'Equality':['Not a Llama']},
+			{'Condensed Milk':['Not a Llama']}
+		]
+	};
 }
