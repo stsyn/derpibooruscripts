1b260cb27bd78baa086a7221bedcd2f8449f4e54
stsyn <stasyan1902@gmail.com>
2018-04-26 15:44:08 +0500

    YDB:ADUp:DeviantArt 0.0.3
    - fxd descriptions

--- YouBooru/YDB-ADUp-DeviantArt.user.js
+++ YouBooru/YDB-ADUp-DeviantArt.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         DeviantArt ADUp Module
-// @version      0.0.2
+// @version      0.0.3
 // @author       stsyn
 // @include      http*://*.deviantart.com/*
 // @require      https://github.com/stsyn/derpibooruscripts/raw/master/YouBooru/lib.js
@@ -24,7 +24,7 @@
 
 	ds.href = '//www.derpibooru.org/images/new?newImg='+encodeURIComponent(document.querySelector('.pimp a.thumb').dataset.superFullImg)+
               '&src='+encodeURIComponent(location.href)+'&tags='+encodeURIComponent('artist:'+document.querySelector('.author .username').innerHTML.toLowerCase())+'&description='+
-              (encodeURIComponent('[bq]'+document.querySelector('.dev-description .text-block')+'[/bq]'))+
+              (encodeURIComponent('[bq]'+document.querySelector('.dev-description .text.block').innerText+'[/bq]'))+
 			  '&newWidth='+document.querySelector('.pimp a.thumb').dataset.superFullWidth+'&newHeight='+document.querySelector('.pimp a.thumb').dataset.superFullHeight;
 	ds.getElementsByTagName('i')[0].style.width = '20px';
 	ds.getElementsByTagName('i')[0].style.background = "url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+CjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmlld0JveD0iMCAwIDE2IDE2Ij48cGF0aCBkPSJtMTMuMiwuNmM1LDkuMi0uMiwxNS41LTIuOSwxNS4zcy0zLjYsLTQuNy05LjIsLTQuNWMyLjYsLTMuNCA4LjMsLS4zIDkuOCwtMS42czMuNywtNS40IDIuMywtOS4yIiBmaWxsPSIjNzNkNmVkIj48L3BhdGg+PHBhdGggZD0ibTExLDAuNi0xLjcsMS40LTEuNywtMS4yLjgsMi0xLjcsMWgxLjhsLTUuNSw5LjcuNCwuMiA1LjUsLTkuNy42LDEuOCAuNSwtMiAyLjIsLS4xLTEuOCwtMS4xLjYsLTIiIGZpbGw9IiM5NGMxZGQiPjwvcGF0aD48Y2lyY2xlIGN4PSIxMy41IiBjeT0iOC41IiBmaWxsPSIjZmZmIiBpZD0iZSIgcj0iMC41Ij48L2NpcmNsZT48dXNlIHg9Ii0yIiB5PSIzIiB4bGluazpocmVmPSIjZSI+PC91c2U+PHVzZSB4PSItNiIgeT0iNCIgeGxpbms6aHJlZj0iI2UiPjwvdXNlPjx1c2UgeD0iLTIuOCIgeT0iNiIgeGxpbms6aHJlZj0iI2UiPjwvdXNlPjwvc3ZnPgo=')";
