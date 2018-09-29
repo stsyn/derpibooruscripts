57c0f9c6815f7e0f581ff8caf995d213157238e9
stsyn <stasyan1902@gmail.com>
2018-09-29 19:52:18 +0500

    Update DerpibooruImitation0.js

--- YouBooru/libs/DerpibooruImitation0.js
+++ YouBooru/libs/DerpibooruImitation0.js
@@ -4,7 +4,108 @@
 *
 */
 
-function updateImages(elem) {    
+function processor(f, a) {
+    let t = {
+        set: function(a, b) {
+            try {
+                return localStorage.setItem(a, JSON.stringify(b)),
+                !0
+            } catch (c) {
+                return !1
+            }
+        },
+        get: function(a) {
+            a = localStorage.getItem(a);
+            try {
+                return JSON.parse(a)
+            } catch (b) {
+                return a
+            }
+        },
+        remove: function(a) {
+            try {
+                return localStorage.removeItem(a),
+                !0
+            } catch (b) {
+                return !1
+            }
+        },
+        watch: function(a, b) {
+            var c = this;
+            window.addEventListener("storage", function(d) {
+                d.key === a && b(c.get(a))
+            })
+        },
+        setWithExpireTime: function(a, b, c) {
+            var d = a + "__lastUpdated";
+            c = Date.now() + c;
+            this.set(a, b) && this.set(d, c)
+        },
+        hasExpired: function(a) {
+            a = this.get(a + "__lastUpdated");
+            return Date.now() > a ? !0 : !1
+        }
+    };
+    let querySelectAll = function (a, b) {
+        void 0 === b && (b = document);
+        a = b.querySelectorAll(a);
+        return [].slice.call(a)
+    }
+    let I = function (a) {
+        return a.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;")
+    }
+    let H = function () {
+        for (var a = [], b = arguments.length; b--; )
+            a[b] = arguments[b];
+        (c = []).concat.apply(c, a).forEach(function(a) {
+            for (; a.firstChild; )
+                a.removeChild(a.firstChild)
+        });
+        var c
+    }
+    let Qa = function (a) {
+        return a.filter(function(a, c, d) {
+            return d.indexOf(a) === c
+        })
+    }
+    let Ra = function (a, b, c) {
+        return b.spoiler_image_uri && c.spoiler_image_uri ? a ? c.images - b.images : b.images - c.images : b.spoiler_image_uri || c.spoiler_image_uri ? b.spoiler_image_uri ? -1 : 1 : c.images - b.images
+    }
+    let Ea = function (a) {
+        var b = t.get("bor_tags_" + a);
+        return b ? b : {
+            id: a,
+            name: "(unknown tag)",
+            images: 0,
+            spoiler_image_uri: null
+        }
+    }
+    let ia = function (a) {
+        var b = a.dataset.size
+          , c = JSON.parse(a.dataset.uris)
+          , d = c[b].replace(/webm$/, "gif")
+          , e = a.querySelector("picture");
+        if (!e)
+            return b = a.dataset.size,
+            b = JSON.parse(a.dataset.uris)[b],
+            (c = a.querySelector("video")) ? (d = a.querySelector("img"),
+            !d || d.classList.contains("hidden") ? a = !1 : (d.classList.add("hidden"),
+            c.innerHTML = '\n    <source src="' + b + '" type="video/webm"/>\n    <source src="' + b.replace(/webm$/, "mp4") + '" type="video/mp4"/>\n  ',
+            c.classList.remove("hidden"),
+            c.play(),
+            a.querySelector(".js-spoiler-info-overlay").classList.add("hidden"),
+            a = !0)) : a = !1,
+            a;
+        e = e.querySelector("img");
+        if (!e || -1 !== e.src.indexOf(d))
+            return !1;
+        t.get("serve_hidpi") && !d.endsWith(".gif") && (e.srcset = d + " 1x, " + c.medium + " 2x");
+        e.src = d;
+        -1 !== c[b].indexOf(".webm") ? (a = a.querySelector(".js-spoiler-info-overlay"),
+        a.classList.remove("hidden"),
+        a.innerHTML = "WebM") : a.querySelector(".js-spoiler-info-overlay").classList.add("hidden");
+        return !0
+    }
 	let unspoilImage = function (a) {
         a.querySelector(".image-filtered").classList.add("hidden");
         a = a.querySelector(".image-show").classList;
@@ -149,6 +250,11 @@ function updateImages(elem) {
 			return unspoilImage(a)
 		})
 	}
-	
-	filterHandler(elem);
+
+    //
+	if (f == 'processImages') filterHandler(a[0]);
+}
+
+function DB_processImages(target) {
+    processor('processImages', [target]);
 }
