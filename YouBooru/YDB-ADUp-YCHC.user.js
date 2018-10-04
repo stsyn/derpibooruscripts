43edbd5744418507979dc28cfa0b4d2615bacd21
stsyn <stasyan1902@gmail.com>
2018-10-04 18:01:50 +0500

    YDB:ADUp:YCHC 0.1.2
    - added slow HQ rescale

--- YouBooru/YDB-ADUp-YCHC.user.js
+++ YouBooru/YDB-ADUp-YCHC.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YCH.Commishes YDB:ADUp module
-// @version      0.1.1
+// @version      0.1.2
 // @author       stsyn
 // @include      https://portfolio.commishes.com/upload/show/*
 // @include      https://ych.commishes.com/followUp/show/*
@@ -24,12 +24,88 @@
     'use strict';
     let mainDomain = 'www.derpibooru.org';
     let stop = 0;
-    let multiplier;
+    let multiplier, hermit;
 
     try {
         mainDomain = unsafeWindow._YDB_public.funcs.getDerpHomeDomain();
     }
     catch(e) {
+    }
+
+    function resample_single(canvas, width, height, resize_canvas) {
+        var width_source = canvas.width;
+        var height_source = canvas.height;
+        width = Math.round(width);
+        height = Math.round(height);
+
+        var ratio_w = width_source / width;
+        var ratio_h = height_source / height;
+        var ratio_w_half = Math.ceil(ratio_w / 2);
+        var ratio_h_half = Math.ceil(ratio_h / 2);
+
+        var ctx = canvas.getContext("2d");
+        var img = ctx.getImageData(0, 0, width_source, height_source);
+        var img2 = ctx.createImageData(width, height);
+        var data = img.data;
+        var data2 = img2.data;
+
+        for (var j = 0; j < height; j++) {
+            for (var i = 0; i < width; i++) {
+                var x2 = (i + j * width) * 4;
+                var weight = 0;
+                var weights = 0;
+                var weights_alpha = 0;
+                var gx_r = 0;
+                var gx_g = 0;
+                var gx_b = 0;
+                var gx_a = 0;
+                var center_y = (j + 0.5) * ratio_h;
+                var yy_start = Math.floor(j * ratio_h);
+                var yy_stop = Math.ceil((j + 1) * ratio_h);
+                for (var yy = yy_start; yy < yy_stop; yy++) {
+                    var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
+                    var center_x = (i + 0.5) * ratio_w;
+                    var w0 = dy * dy; //pre-calc part of w
+                    var xx_start = Math.floor(i * ratio_w);
+                    var xx_stop = Math.ceil((i + 1) * ratio_w);
+                    for (var xx = xx_start; xx < xx_stop; xx++) {
+                        var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
+                        var w = Math.sqrt(w0 + dx * dx);
+                        if (w >= 1) {
+                            //pixel too far
+                            continue;
+                        }
+                        //hermite filter
+                        weight = 2 * w * w * w - 3 * w * w + 1;
+                        var pos_x = 4 * (xx + yy * width_source);
+                        //alpha
+                        gx_a += weight * data[pos_x + 3];
+                        weights_alpha += weight;
+                        //colors
+                        if (data[pos_x + 3] < 255)
+                            weight = weight * data[pos_x + 3] / 250;
+                        gx_r += weight * data[pos_x];
+                        gx_g += weight * data[pos_x + 1];
+                        gx_b += weight * data[pos_x + 2];
+                        weights += weight;
+                    }
+                }
+                data2[x2] = gx_r / weights;
+                data2[x2 + 1] = gx_g / weights;
+                data2[x2 + 2] = gx_b / weights;
+                data2[x2 + 3] = gx_a / weights_alpha;
+            }
+        }
+        //clear and resize canvas
+        if (resize_canvas === true) {
+            canvas.width = width;
+            canvas.height = height;
+        } else {
+            ctx.clearRect(0, 0, width_source, height_source);
+        }
+
+        //draw
+        ctx.putImageData(img2, 0, 0);
     }
 
     function b64toBlob(b64Data, contentType, sliceSize) {
@@ -106,10 +182,12 @@
 
             image.onload = function() {
                 e.target.innerHTML = 'Scroll down';
-                let canvas = InfernoAddElem('canvas',{width:width,height:height, style:{maxWidth:'100%'}}, []);
+                hermit = document.getElementById('_adup_hermit').checked;
+                let canvas = InfernoAddElem('canvas',{width:(hermit?width*multiplier:width),height:(hermit?height*multiplier:height), style:{maxWidth:'100%'}}, []);
                 document.body.appendChild(canvas);
                 let ctx = canvas.getContext('2d');
-                ctx.drawImage(image, 0, 0, width, height);
+                ctx.drawImage(image, 0, 0, (hermit?width*multiplier:width), (hermit?height*multiplier:height));
+                if (hermit) resample_single(canvas,width,height,true);
                 alert('Save image from the bottom and upload it manually. Sorry, right now it\'s the only way to get adequate quality file :(');
                 /*
                 I tried to do something automatically.
@@ -132,8 +210,11 @@
         }}]}, []));
         holder.appendChild(InfernoAddElem('br', {},[]));
         holder.appendChild(InfernoAddElem('br', {},[]));
-        holder.appendChild(InfernoAddElem('span', {innerHTML:'JPEG size (2-4 recommended) '},[]));
+        holder.appendChild(InfernoAddElem('span', {innerHTML:'JPEG size (2-4 recommended) ', style:'font-size:80%'},[]));
         holder.appendChild(InfernoAddElem('input', {type:'number',id:'_adup_quality',style:{width:'3em'},value:2},[]));
+        holder.appendChild(InfernoAddElem('label', {innerHTML:' HQ rescale ', style:'font-size:80%'},[
+            InfernoAddElem('input', {type:'checkbox',id:'_adup_hermit',checked:true},[])
+        ]));
     }
     else {
         // handle derpibooru
