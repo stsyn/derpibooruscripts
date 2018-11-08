f7baf975eb715a2bed45ac52b557be5290553869
stsyn <stasyan1902@gmail.com>
2018-11-08 19:45:05 +0500

    Update SettingsData0.js

--- YouBooru/libs/SettingsData0.js
+++ YouBooru/libs/SettingsData0.js
@@ -1,3 +1,4 @@
 function getDonateLink() {
-	return 'https://ko-fi.com/C0C8BVXS';
+	let donationLinks = ['https://ko-fi.com/C0C8BVXS', 'https://www.paypal.me/stsyn', 'https://steamcommunity.com/id/stsyn/wishlist/','https://money.yandex.ru/to/410014956085536']
+	return donationLinks[parseInt(Math.random() * donationLinks.length)];
 }
