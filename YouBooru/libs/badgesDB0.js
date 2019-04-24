4435c1d1742d02294d30bdb06634c2bd3e8e069e
stsyn <stasyan1902@gmail.com>
2019-04-24 09:49:23 +0500

    Update badgesDB0.js

--- YouBooru/libs/badgesDB0.js
+++ YouBooru/libs/badgesDB0.js
@@ -2,13 +2,17 @@ function getBadgesImplications() {
 	return {
 		normal:{
 			'Hard Work':['Perfect Pony Plot Provider','Artist'],
+			'HARD WERK':['Perfect Pony Plot Provider','Artist'],
 			'Best Artist':['Artist'],
 			'Da Magicks!':['Fine Arts','Artist'],
+
 			'An Artist Who Rocks':['Artist'],
 			'A Really Classy Artist':['An Artist Who Rocks','Artist'],
 			'A Really Hyper Artist':['A Really Classy Artist','An Artist Who Rocks','Artist'],
 			'Chaotic Little Trees':['A Really Hyper Artist','A Really Classy Artist','An Artist Who Rocks','Artist'],
+
 			'Cool Crow':['Magnificent Metadata Maniac'],
+
 			'Bronze Patron':['Happy Derpy'],
 			'Silver Patron':['Happy Derpy'],
 			'Gold Patron':['Happy Derpy'],
@@ -18,8 +22,13 @@ function getBadgesImplications() {
 			'Friendship, Art, and Magic (6 Years)':['Friendship, Art, and Magic (5 Years)'],
 			'Friendship, Art, and Magic (7 Years)':['Friendship, Art, and Magic (5 Years)','Friendship, Art, and Magic (6 Years)'],
 			'Helpful Owl':['Friendly Griffon'],
+			'Toola Roola':['Friendly Griffon', 'Helpful Owl'],
+
 			'Equality':['Not a Llama'],
 			'Condensed Milk':['Not a Llama'],
+			'Wallet After Summer Sale':['Not a Llama', 'Equality', 'Condensed Milk'],
+			'Economist':['Not a Llama', 'Equality', 'Condensed Milk'],
+
 			'A Tale For The Ages':['Birthday Cake']
 		},
 		donations:{
@@ -27,6 +36,7 @@ function getBadgesImplications() {
 			'Gold Patron':['Emerald','Ruby','Sapphire','Diamond','Heart Gem','Element of Generosity','Silver Bit','Bronze Bit'],
 			'Silver Patron':['Emerald','Ruby','Sapphire','Diamond','Heart Gem','Element of Generosity','Bronze Bit'],
 			'Bronze Patron':['Emerald','Ruby','Sapphire','Diamond','Heart Gem','Element of Generosity'],
+
 			'Platinum Bit':['Emerald','Ruby','Sapphire','Diamond','Heart Gem','Element of Generosity','Platinum Patron','Gold Patron','Silver Patron','Bronze Patron'],
 			'Gold Bit':['Emerald','Ruby','Sapphire','Diamond','Heart Gem','Element of Generosity','Gold Patron','Silver Patron','Bronze Patron'],
 			'Silver Bit':['Emerald','Ruby','Sapphire','Diamond','Heart Gem','Element of Generosity','Silver Patron','Bronze Patron'],
