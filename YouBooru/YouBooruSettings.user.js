16c2670e704f01ede83366254740571b14fd2ec3
stsyn <stasyan1902@gmail.com>
2019-07-22 08:47:06 +0500

    YDB:S 0.9.22a

--- YouBooru/YouBooruSettings.user.js
+++ YouBooru/YouBooruSettings.user.js
@@ -942,7 +942,9 @@
 	////////////////////////////
 
 	function YB_createEmpty() {
-		if (document.querySelector('#content h1').innerHTML != 'Schrödinger\'s Pony') return;
+		if (document.querySelector('#content h1').innerHTML != 'API') return;
+        document.getElementById('content').removeChild(document.querySelector('#content>p'));
+        document.getElementById('content').removeChild(document.querySelector('#content>a'));
 		document.querySelector('#content h1').innerHTML = 'Derp!';
 		document.querySelector('#content .walloftext').innerHTML = '<p>I know the script is callled "YourBooru", but as much as I would want it to be truly yours, I could not find a page you specified. Nope. Nothing at all! I tried though.</p><p>Make sure that the URL you are trying to access is valid and that you aren\'t trying to hunt ghosts here. Otherwise you might have hit a bug and it would be a good idea to report it to me!</p>';
 	}
