d8d679f0e9239fe297e613f41b9d91b03df19e5c
stsyn <stasyan1902@gmail.com>
2019-08-09 17:36:54 +0500

    Update lib.js

--- YouBooru/lib.js
+++ YouBooru/lib.js
@@ -25,17 +25,20 @@ function InfernoAddElem(tag, values, childs) {
 	}
 	else t = document.createElement(tag);
 
-	for (var i in values) if (i!='events' && i!='dataset' && i!='innerHTML' && i!='checked' && i!='disabled' && i!='value' && i!='selected' && i!='className' && !(i=='style' && typeof values.style=='object')) t.setAttribute(i,values[i]);
-	if (values.dataset != undefined) for (var i in values.dataset) t.dataset[i] = values.dataset[i];
-	if (values.className != undefined) t.className = values.className;
-	if (values.innerHTML != undefined) t.innerHTML = values.innerHTML;
-	if (values.value != undefined) t.value = values.value;
-	if (values.checked != undefined) t.checked = values.checked;
-	if (values.selected != undefined) t.selected = values.selected;
-	if (values.disabled != undefined) t.disabled = values.disabled;
-	if (values.events != undefined) values.events.forEach(function(v,i,a) {t.addEventListener(v.t, v.f);});
-	if (typeof values.style=='object') for (var i in values.style) t.style[i] = values.style[i];
+	for (let i in values) if (i!='events' && i!='dataset' && i!='innerHTML' && i!='checked' && i!='disabled' && i!='value' && i!='selected' && i!='className' && !(i=='style' && typeof values.style=='object')) t.setAttribute(i,values[i]);
+	if (values.dataset !== undefined) for (let i in values.dataset) t.dataset[i] = values.dataset[i];
+	if (values.className !== undefined) t.className = values.className;
+	if (values.innerHTML !== undefined) t.innerHTML = values.innerHTML;
+	if (values.value !== undefined) t.value = values.value;
+	if (values.checked !== undefined) t.checked = values.checked;
+	if (values.selected !== undefined) t.selected = values.selected;
+	if (values.disabled !== undefined) t.disabled = values.disabled;
+	if (values.events !== undefined) {
+        if (Array.isArray(values.events)) values.events.forEach(function(v,i,a) {t.addEventListener(v.t, v.f);});
+        else for (let i in values.events) t.addEventListener(i, values.events[i]);
+    }
+	if (typeof values.style == 'object') for (let i in values.style) t.style[i] = values.style[i];
 
-	if (childs != undefined && childs.length != undefined) childs.forEach(function(c,i,a) {t.appendChild(c.parentNode == null?c:c.cloneNode(true));});
+	if (childs && childs.length != undefined) childs.forEach(function(c,i,a) {t.appendChild(c.parentNode == null?c:c.cloneNode(true));});
 	return t;
 }
