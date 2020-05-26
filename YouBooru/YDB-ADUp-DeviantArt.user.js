85d4497eb41a4e56298bf085f0c37da5f43d8785
stsyn <stasyan1902@gmail.com>
2020-05-26 15:59:27 +0500

    YDB:ADUp:DA 0.3
    - eclipse fix

--- YouBooru/YDB-ADUp-DeviantArt.user.js
+++ YouBooru/YDB-ADUp-DeviantArt.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         DeviantArt ADUp Module
-// @version      0.2.8
+// @version      0.3.0
 // @author       stsyn
 // @include      http*://*.deviantart.com/*
 // @require      https://github.com/stsyn/derpibooruscripts/raw/master/YouBooru/libs/CreateElement.js
 // @require      https://github.com/stsyn/derpibooruscripts/raw/master/YouBooru/lib.js
 // @downloadURL  https://github.com/stsyn/derpibooruscripts/raw/master/YouBooru/YDB-ADUp-DeviantArt.user.js
 // @grant        unsafeWindow
-// @grant        GM_xmlhttpRequest
 // @grant        GM_download
 // ==/UserScript==
 
@@ -17,22 +16,20 @@
   const leak = true;
   const derpImage = "url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+CjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmlld0JveD0iMCAwIDE2IDE2Ij48cGF0aCBkPSJtMTMuMiwuNmM1LDkuMi0uMiwxNS41LTIuOSwxNS4zcy0zLjYsLTQuNy05LjIsLTQuNWMyLjYsLTMuNCA4LjMsLS4zIDkuOCwtMS42czMuNywtNS40IDIuMywtOS4yIiBmaWxsPSIjNzNkNmVkIj48L3BhdGg+PHBhdGggZD0ibTExLDAuNi0xLjcsMS40LTEuNywtMS4yLjgsMi0xLjcsMWgxLjhsLTUuNSw5LjcuNCwuMiA1LjUsLTkuNy42LDEuOCAuNSwtMiAyLjIsLS4xLTEuOCwtMS4xLjYsLTIiIGZpbGw9IiM5NGMxZGQiPjwvcGF0aD48Y2lyY2xlIGN4PSIxMy41IiBjeT0iOC41IiBmaWxsPSIjZmZmIiBpZD0iZSIgcj0iMC41Ij48L2NpcmNsZT48dXNlIHg9Ii0yIiB5PSIzIiB4bGluazpocmVmPSIjZSI+PC91c2U+PHVzZSB4PSItNiIgeT0iNCIgeGxpbms6aHJlZj0iI2UiPjwvdXNlPjx1c2UgeD0iLTIuOCIgeT0iNiIgeGxpbms6aHJlZj0iI2UiPjwvdXNlPjwvc3ZnPgo=')";
 
-  let l, width, height, i=0, dunno, hasFull, eclipse = false, artist;
+  let l, width, height, artist;
   var req = false;
 
   function spawn(link, text) {
     let target, target2, target3;
-    if (eclipse) {
-      target = document.querySelector('._3fcE7._15yPL.fXGZA');
-      target2 = target.querySelector('._ydb_adup') || document.createElement('div');
-      target2.className = 'JQhXs _ydb_adup';
-      target.insertBefore(target2, target.children[0]);
-      target3 = document.createElement('div');
-      target3.className = '_2iAFC';
-      target2.appendChild(target3);
-    } else {
-      target = Array.from(document.getElementsByClassName('dev-meta-actions')).pop();
-    }
+
+    target = document.querySelector('._3fcE7._15yPL.fXGZA');
+    target2 = target.querySelector('._ydb_adup') || document.createElement('div');
+    target2.className = 'JQhXs _ydb_adup';
+    target.insertBefore(target2, target.children[0]);
+    target3 = document.createElement('div');
+    target3.className = '_2iAFC';
+    target2.appendChild(target3);
+
     let href = '//'+mainDomain+'/images/new?newImg='+encodeURIComponent(link)+
       '&src='+encodeURIComponent(location.href)+
       '&tags='+encodeURIComponent('artist:'+artist)+
@@ -41,45 +38,28 @@
       '&newHeight='+height;
 
     let ds = createElement('a', {
-      className: eclipse ? 'V7yJQ' : 'dev-page-button dev-page-button-with-text dev-page-download',
+      className: 'V7yJQ',
       target: '_blank',
       href,
-      style: hasFull ? '' : 'width:calc(50% - 18px);display:inline-block;margin-right:4px'
+      style: 'width:calc(50% - 18px);display:inline-block;margin-right:4px'
     }, [
       createElement('i', {style: {width: '20px', background: derpImage}}),
       createElement('span.label', ' Upload on DB'),
-      createElement('span.text', (eclipse ? ' ' : '') + text),
+      createElement('span.text', ' ' + text),
     ]);
 
-    i = 1;
-    if (eclipse) {
-      target3.appendChild(ds);
-    } else {
-      target.appendChild(ds);
-    }
-
-    if (!hasFull) {
-      let ls = ds.cloneNode(true);
-      ls.href = link;
-      ls.getElementsByTagName('i')[0].style.background = "url(https://st.deviantart.net/minish/deviation/action-sprites.png?)";
-      ls.getElementsByTagName('i')[0].style.backgroundPosition = '-1px -78px';
-      ls.querySelector('span.label').innerHTML = " Direct link";
-      ls.addEventListener('click', (e) => {
-        e.preventDefault();
-        GM_download(link, link.split('/').pop().split('?')[0])
-      });
-      if (eclipse) {
-        ls.querySelector('span.text').innerHTML = ' '+text;
-      } else {
-        ls.querySelector('span.text').innerHTML = text;
-      }
-      if (eclipse) {
-        target3.appendChild(ls);
-      } else {
-        target.appendChild(ls);
-      }
-    }
-
+    target3.appendChild(ds);
+    let ls = ds.cloneNode(true);
+    ls.href = link;
+    ls.getElementsByTagName('i')[0].style.background = "url(https://st.deviantart.net/minish/deviation/action-sprites.png?)";
+    ls.getElementsByTagName('i')[0].style.backgroundPosition = '-1px -78px';
+    ls.querySelector('span.label').innerHTML = " Direct link";
+    ls.addEventListener('click', (e) => {
+      e.preventDefault();
+      GM_download(link, link.split('/').pop().split('?')[0])
+    });
+    ls.querySelector('span.text').innerHTML = ' '+text;
+    target3.appendChild(ls);
     target.classList.add('patched');
   }
 
@@ -89,61 +69,52 @@
       code = parseInt(location.href.split('/').pop()).toString(36);
     }
     document.getElementById('deviantART-v5').classList.add('patched');
-    document.getElementById('description').appendChild(InfernoAddElem('a', {href:`http://orig01.deviantart.net/x_by_x-d${code}.png`, innerHTML:'https://fav.me/d'+code}));
+    document.getElementById('description').appendChild(createElement('a', {href:`http://orig01.deviantart.net/x_by_x-d${code}.png`}, 'https://fav.me/d'+code));
   }
 
   function DA(p) {
-    dunno = p;
     req = true;
     if (l.indexOf('/art/') == -1) {
       console.log('Not art');
       return;
     }
-
-    hasFull = false; //!!(document.querySelector('.dev-meta-actions:not(.patched) .dev-page-download'));
     let dlink, text = '';
-    if (hasFull) {
-      let xlink = document.querySelector('.dev-meta-actions:not(.patched) .dev-page-download').href;
-      width = document.querySelector('.dev-meta-actions:not(.patched) div.dev-metainfo-details dl dd:nth-child(6)').innerHTML.split('×')[0];
-      height = document.querySelector('.dev-meta-actions:not(.patched) div.dev-metainfo-details dl dd:nth-child(6)').innerHTML.split('×')[1];
 
-      dlink = location.href;
-    } else {
-      const o = Array.from(document.querySelectorAll('.pimp a.thumb')).pop();
-      if (eclipse) {
-        dlink = document.querySelector('._1izoQ.vbgSM').src;
-        artist = document.querySelector('[data-hook="deviation_meta"] a[data-hook="user_link"]').dataset.username.toLowerCase();
-      } else {
-        dlink = o.dataset.superFullImg;
-        artist = document.querySelector('.author .username').innerHTML.toLowerCase();
-      }
+    const o = Array.from(document.querySelectorAll('.pimp a.thumb')).pop();
+    artist = document.querySelector('[data-hook="deviation_meta"] a[data-hook="user_link"]').dataset.username.toLowerCase();
+
+    const url = `https://www.deviantart.com/_napi/shared_api/deviation/extended_fetch?deviationid=${location.href.split('-').pop()}&type=art`;
+    fetch(url)
+    .then(resp => resp.json())
+    .then(resp => {
+      const data = resp.deviation.media;
 
       // failsafe
-      const isSourcePng = /https:\/\/images-wixmp-[0-9a-f]+\.wixmp\.com\/f\/[0-9a-f\-]+\/[0-9a-z\-]+\.png/.test(dlink);
-      const isSourceJpg = /https:\/\/images-wixmp-[0-9a-f]+\.wixmp\.com\/f\/[0-9a-f\-]+\/[0-9a-z\-]+\.jpg/.test(dlink);
-      if (isSourcePng) {
-        dlink = dlink.replace(/(https:\/\/[0-9a-z\-\.]+\/f\/[0-9a-f\-]+\/[0-9a-z\-]+\.png\/v1\/fill\/[0-9a-z_,]+\/[0-9a-z_\-]+)(\.jpg)(.*)/,'$1.png$3');
+      const isSourcePng = data.baseUri.endsWith('.png');
+      const fullview = data.types.find(item => item.t === 'fullview');
+      if (fullview && fullview.c) {
+        if (isSourcePng) {
+          dlink = data.baseUri + `/v1/fill/w_${fullview.w},h_${fullview.h},q_100,strp/${data.prettyName}-pre.png?token=` + data.token[0];
+        } else {
+          dlink = data.baseUri + `/v1/fill/w_${fullview.w},h_${fullview.h},q_100,strp/${data.prettyName}-pre.jpg?token=` + data.token[0];
+        }
       } else {
-        dlink = dlink.replace(/(https:\/\/[0-9a-z\-\.]+\/f\/[0-9a-f\-]+\/[0-9a-z\-]+\.jpg\/v1\/fill\/w_[0-9]+,h_[0-9]+,q_)([0-9]+)(,[a-z]+\/[a-z0-9_\-]+\.jpg.*)/,'$1100$3');
+          dlink = data.baseUri + '?token=' + data.token[0];
       }
       spawn(dlink, 'failsafe');
 
       // intermediary
-      if (eclipse) {
-        dlink = document.querySelector('._1izoQ.vbgSM').src.replace(/\/v1\/fill.*$/, '').replace('.com/f/', '.com/intermediary/f/').replace(/\?token=.*$/, '');
-      } else {
-        dlink = o.dataset.superFullImg.replace(/\/v1\/fill.*$/, '').replace('.com/f/', '.com/intermediary/f/').replace(/\?token=.*$/, '');
-      }
+      dlink = document.querySelector('._1izoQ.vbgSM').src.replace(/\/v1\/fill.*$/, '').replace('.com/f/', '.com/intermediary/f/').replace(/\?token=.*$/, '');
+
       spawn(dlink, 'intermediary');
+    })
 
-      // leak
-      const code = parseInt(location.href.split('-').pop()).toString(36);
-      dlink = `http://orig01.deviantart.net/x_by_x-d${code}.png`;
-      spawn(dlink, "leak");
-    }
+    // leak
+    /*const code = parseInt(location.href.split('-').pop()).toString(36);
+    dlink = `http://orig01.deviantart.net/x_by_x-d${code}.png`;
+    spawn(dlink, "leak");
 
-    if (document.querySelector('.dev-description .text.block')) text = '[bq]'+document.querySelector('.dev-description .text.block').innerText+'[/bq]';
-
+    if (document.querySelector('.dev-description .text.block')) text = '[bq]'+document.querySelector('.dev-description .text.block').innerText+'[/bq]';*/
   }
 
   function DA_check() {
@@ -157,18 +128,13 @@
     if (document.querySelector('#deviantART-v5.error-404:not(.patched)')) {
       l = window.location.pathname;
       DA404();
-    }
-    else if (document.querySelector('.dev-meta-actions:not(.patched)')) {
-      l = window.location.pathname;
-      DA(i);
     } else if (document.querySelector('._3fcE7._15yPL.fXGZA:not(.patched)')) {
-      eclipse = true;
       l = window.location.pathname;
-      DA(i);
-    } else if (eclipse && l != window.location.pathname) {
-      document.querySelector('._ydb_adup').innerHTML = '';
+      DA();
+    } else if (l != window.location.pathname) {
+      if (document.querySelector('._ydb_adup')) document.querySelector('._ydb_adup').innerHTML = '';
       l = window.location.pathname;
-      DA(i);
+      DA();
     }
   }
 
