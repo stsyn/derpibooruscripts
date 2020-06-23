ad4e826a93aeae93971db4882703378889e9c3e6
stsyn <stasyan1902@gmail.com>
2020-06-23 23:55:03 +0500

    MT: 0.1.12

--- YouBooru/MT.user.js
+++ YouBooru/MT.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YDB:MT
-// @version      0.1.11
+// @version      0.1.12
 // @author       stsyn
 
 // @include      /http[s]*:\/\/(www\.|)(trixie|derpi)booru.org\/.*/
@@ -111,7 +111,6 @@
         c.dataset.index = i;
         for (let j=0; j<c.querySelectorAll('.image-container').length; j++) {
           const el = c.querySelectorAll('.image-container')[j];
-          //if (el.querySelector('.media-box__overlay.js-spoiler-info-overlay').innerHTML == '') continue;
           if (unsafeWindow.booru.spoilerType == 'click') el.querySelector('a').addEventListener('click', e => despoil(e, 'click'));
           if (unsafeWindow.booru.spoilerType == 'hover') el.querySelector('a').addEventListener('mouseenter', e => despoil(e, 'mouseenter'));
           el.querySelector('a').addEventListener('mouseleave', e => despoil(e, 'mouseleave'));
@@ -181,24 +180,142 @@
     //more to come
   }
 
-  function sortAliases() {
-    if (!(location.pathname.split('/').pop() === 'aliases')) return;
-    const container = document.querySelector('table');
-    const lines = Array.from(container.querySelectorAll('tbody tr'))
-    .sort((a, b) => {
-      const methods = ['IP + FP', 'IP', 'FP'];
-      const methodA = methods.indexOf(a.childNodes[1].innerText);
-      const methodB = methods.indexOf(b.childNodes[1].innerText);
-      if (methodA < methodB) return -1;
-      if (methodA > methodB) return 1;
-      const timeA = a.childNodes[2].childNodes[0].dateTime;
-      const timeB = b.childNodes[2].childNodes[0].dateTime;
-      if (timeA < timeB) return 1;
-      if (timeA > timeB) return -1;
-      return 0;
+  function handleAliases() {
+    const href = location.pathname.split('/');
+    if (!(href.pop() === 'aliases')) return;
+    const profileUrl = href.pop();
+
+    const now = new Date();
+    const recentString = (now.getFullYear() - 1).toString() + '-' + (now.getMonth() + 1).toString().padStart(2, '0') + '-' + (now.getDate()).toString().padStart(2, '0');
+    const methods = ['IP + FP', 'IP', 'FP'];
+    const container = document.getElementById('content');
+    const renderPlace = createElement('div');
+    const parser = new DOMParser();
+    const userUrlCache = new Map();
+    let renderPlaceData = [];
+
+    function prepareRenderPlace() {
+      renderPlaceData = methods.map((name, index) => {
+        let areaTop, areaHidden;
+        renderPlace.appendChild(createElement('div', [
+          ['h4', name],
+          ['table.table', [
+            renderTableHead(),
+            areaTop = createElement('tbody')]
+          ],
+          ['input.toggle-box', {id: '_alias_'+index, type: 'checkbox'}],
+          ['label', {for: '_alias_'+index}, 'Older collisions'],
+          ['div.toggle-box-container', [
+            ['table.table', [
+              renderTableHead(),
+              areaHidden = createElement('tbody')]
+            ]
+          ]]
+        ]));
+        return {areaTop, areaHidden}
+      });
+    }
+
+    function renderTableHead() {
+      return ['thead', [
+        ['tr', [
+          ['td', 'User'],
+          ['td', 'Creation Date'],
+          ['td', 'Ban Status'],
+          ['td', 'IP Collision Date'],
+          ['td', 'FP Collision Date'],
+        ]]
+      ]];
+    }
+
+    function makeTableLine(item) {
+      return createElement('tr', [item.user, item.date, item.ban, ['td', item.ipCollision], ['td', item.fpCollision]]);
+    }
+
+    function renderData(data) {
+      data.reduce((acc, item) => {
+        const methodId = methods.indexOf(item.method);
+        if (methodId === -1) methodId = 2;
+        acc[methodId].push(item);
+        return acc;
+      }, [[], [], []])
+        .forEach((items, index) => {
+        const listing = items.sort((a, b) => {
+          if (a.ipCollision < b.ipCollision) return 1;
+          if (a.ipCollision > b.ipCollision) return -1;
+          if (a.fpCollision < b.fpCollision) return 1;
+          if (a.fpCollision > b.fpCollision) return -1;
+          const timeA = a.date.childNodes[0].dateTime;
+          const timeB = b.date.childNodes[0].dateTime;
+          if (timeA < timeB) return 1;
+          if (timeA > timeB) return -1;
+          return 0;
+        }).reduce((acc, item) => {
+          if ((!item.ipCollision || item.ipCollision.split('/') < recentString) &&
+             (!item.fpCollision || item.fpCollision.split('/') < recentString)) {
+            acc[1].push(item);
+          }
+          else acc[0].push(item);
+          return acc;
+        }, [[], []]).map(itemGroup => {
+          return itemGroup.map(makeTableLine);
+        });
+        const areaTop = renderPlaceData[index].areaTop;
+        const areaHidden = renderPlaceData[index].areaHidden;
+        clearElement(areaTop);
+        clearElement(areaHidden);
+        fillElement(areaTop, listing[0]);
+        fillElement(areaHidden, listing[1]);
+      });
+    }
+
+    const lines = Array.from(container.querySelectorAll('tbody tr')).map(item => {
+      const data = {
+        user: item.childNodes[0],
+        userName: item.childNodes[0].innerText,
+        method: item.childNodes[1].innerText,
+        date: item.childNodes[2],
+        ban: item.childNodes[3],
+        ipCollision: item.childNodes[1].innerText.includes('IP') ? 'loading...' : '',
+        fpCollision: item.childNodes[1].innerText.includes('FP') ? 'loading...' : ''
+      };
+
+      userUrlCache.set(data.user.childNodes[0].href.split('/').pop(), data);
+
+      return data;
     });
-    container.removeChild(container.querySelector('tbody'));
-    container.appendChild(createElement('tbody', lines));
+
+    function collisionParse(content, field) {
+      Array.from(content.querySelectorAll('#content>ul>li')).forEach(item => {
+        if (item.querySelectorAll('ul>li').length > 1) {
+          const originCollision = item.querySelector(`li>a[href*="profiles/${profileUrl}"]`).parentNode.querySelector('time').dateTime.split('T')[0];
+          Array.from(item.querySelectorAll('ul>li')).forEach(line => {
+            const userUri = line.querySelector('a').href.split('/').pop();
+            if (userUri === profileUrl) return;
+            userUrlCache.get(userUri)[field] = originCollision + '/' + line.querySelector('time').dateTime.split('T')[0];
+          });
+        }
+      });
+      renderData(lines);
+    }
+
+    fetchJson('GET', `/profiles/${profileUrl}/ip_history`)
+    .then(response => response.text())
+    .then(resp => {
+      const doc = createElement('div', {innerHTML: resp});
+      collisionParse(doc, 'ipCollision');
+    });
+    fetchJson('GET', `/profiles/${profileUrl}/fp_history`)
+    .then(response => response.text())
+    .then(resp => {
+      const doc = createElement('div', {innerHTML: resp});
+      collisionParse(doc, 'fpCollision');
+    });
+
+    prepareRenderPlace();
+    container.removeChild(container.querySelector('table'));
+    container.appendChild(renderPlace);
+    renderData(lines);
   }
 
   try {ajaxDupes();} catch(e) {error("ajaxDupes", e)};
@@ -206,5 +323,5 @@
   setTimeout(() => {try {syncDupesDespoil();} catch(e) {error("syncDupesDespoil", e)};}, 300);
   try {hideTools();} catch(e) {error("hideTools", e)};
   try {isThingReported();} catch(e) {error("isThingReported", e)};
-  try {sortAliases();} catch(e) {error("sortAliases", e)};
+  try {handleAliases();} catch(e) {error("handleAliases", e)};
 })();
