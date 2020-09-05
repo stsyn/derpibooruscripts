16804286f6e834d28f95ef51cc145b47e80544ec
stsyn <stasyan1902@gmail.com>
2020-09-05 23:44:43 +0500

    Update mbUp-sites.js

--- YouBooru/libs/dbs/mbUp-sites.js
+++ YouBooru/libs/dbs/mbUp-sites.js
@@ -1,3 +1,7 @@
 function GetMBUpRulesets() {
-  
+  return (origin, target) => {
+    if (origin === 'derpibooru.org' && target === 'furbooru.org') {
+      return GetDerpi2FurbooruRuleset();
+    }
+  };
 }
