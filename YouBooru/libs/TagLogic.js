a4a47b827be6d55af5eb465e7920edaffbd5325b
stsyn <stasyan1902@gmail.com>
2020-09-05 23:09:54 +0500

    Update TagLogic.js

--- YouBooru/libs/TagLogic.js
+++ YouBooru/libs/TagLogic.js
@@ -3,9 +3,13 @@
  */
 var YDB_api = YDB_api || {};
 (function() {
-  // - rules be like (can have ^ to act negative)
+  const rulesetVersionField = '[{[version]}]';
+  function __unique(item, index, array) {
+    return index === array.indexOf(item);
+  }
+
+  //// - rules be like (can have ^ to act negative)
   // safe
-  // solo+female     — combine (AND)
   // -pony           — negate
   // _origin         — category
   // *sparkle        — wildcard only 1 * supported
@@ -14,25 +18,65 @@ var YDB_api = YDB_api || {};
   // *               — any tag
   // __              — no tag
 
-  // - actions be like
+  // solo+female     — combine (AND)
+
+  // pegasus,unicorn — listing (it's not global OR!)
+  // pony,!anthro    — exclusion (it's not global NOT!)
+  // artist:*,idw[[1]] — use [[]] for count cases for listings and exclusions
+
+  //// - actions be like
   // safe            — adds
   // -safe           — remove tag
   // -[x]            — remove tags from x + match
   // ? (mlp)
   // ? (mlp)[x]      — removes part
   // %* (mlp)
+  // %solo *
   // %* (mlp)[x]     — appends part to every tag
-  // ^error text     — throws error (can't have + in error text)
-  // explicit+sex    — combine actions (AND)
+  // ^error text     — throws error
+
+  // party+pinkie pie — combine actions (AND)
+
+  // Actions order is global for whole ruleset iteration:
+  // 1. Tag removals;
+  // 2. Part of tag removals;
+  // 3. Part of tag appends;
+  // 4. Tag appends.
 
   YDB_api.applyRulesetOnTags = function(ruleset, tagArray, dontApply) {
+    const version = 0;
+    if (ruleset[rulesetVersionField] && parseInt(ruleset[rulesetVersionField].split('.')[1]) > version) {
+      throw new Error('Unsupported ruleset version');
+    }
     const tagsToRemove = new Set();
     const tagsToAdd = new Set();
     const renamesDeletions = {};
     const renamesAdditions = {};
     const errors = [];
 
-    function getMatchingTags(rulePart, tagArray) {
+    function getMatchingTags(rule, tagArray) {
+      function checkTag(rulePart) {
+        if (rulePart === '__') return [];
+        if (rulePart === '*') return tagArray;
+
+        // category
+        if (rulePart.startsWith('_')) {
+          const rule = rulePart.match(/_(.*)(\[.?\d+]|)/)[1];
+          const match = tagArray.filter(tag => tag.category === rule);
+          return checkCondition(rulePart, match);
+        }
+        // wildcard
+        if (rulePart.indexOf('*') > -1) {
+          const rule = rulePart.match(/(.*)(\[.?\d+]|)/)[1];
+          const parts = rule.split('*');
+          const match = tagArray.filter(tag => tag.name.startsWith(parts[0]) && tag.name.endsWith(parts[1]))
+          return checkCondition(rulePart, match);
+        }
+        // strict
+        const match = tagArray.filter(tag => tag.name === rulePart);
+        return checkCondition(rulePart, match);
+      }
+
       function checkCondition(rule, match) {
         const condition = rule.match(/\[(.?)(\d+)]/);
         if (condition) {
@@ -46,50 +90,52 @@ var YDB_api = YDB_api || {};
         return match;
       }
 
-      if (rulePart === '__') return [];
-      if (rulePart === '*') return tagArray;
-      rulePart = rulePart.replace(/\[\[plus]]/g, '+');
+      function checkConditionGlobal(rule, match) {
+        const condition = rule.match(/\[\[(.?)(\d+)]]/);
+        if (condition) {
+          const value = parseInt(condition[2]);
+          switch (condition[1]) {
+            case '>': return (match.length > value) ? match : [];
+            case '<': return (match.length < value) ? match : [];
+            case '': return (match.length === value) ? match : [];
+          }
+        }
+        return match;
+      }
 
-      // category
-      if (rulePart.startsWith('_')) {
-        const rule = rulePart.match(/_(.*)(\[.?\d+]|)/)[1];
-        const match = tagArray.filter(tag => tag.category === rule);
-        console.warn(match, tagArray);
-        return checkCondition(rulePart, match);
+      rule = rule.replace(/\[\[plus]]/g, '+');
+      if (rule.indexOf(',') > -1) {
+        const rules = rule.split(',');
+        const matched = rules.filter(rule => !rule.startsWith('!').map(checkTag)).flat().filter(__unique);
+        const notMatched = rules.filter(rule => rule.startsWith('!').map(rule => checkTag(rule.substring(1)))).flat();
+        const match = matched.filter(tag => !notMatched.includes(tag));
+        return checkConditionGlobal(rule, match);
+      } else {
+        return checkTag(rule);
       }
-      // wildcard
-      if (rulePart.indexOf('*') > -1) {
-        const rule = rulePart.match(/(.*)(\[.?\d+]|)/)[1];
-        const parts = rule.split('*');
-        const match = tagArray.filter(tag => tag.name.startsWith(parts[0]) && tag.name.endsWith(parts[1]))
-        return checkCondition(rulePart, match);
-      }
-      // strict
-      const match = tagArray.filter(tag => tag.name === rulePart);
-      return checkCondition(rulePart, match);
     }
 
     function applyTagRule(action, tags, rules) {
+      action = action.replace(/\[\[plus]]/g, '+');
       // removals
       if (action.startsWith('-')) {
         action = action.substring(1);
         const linked = action.match(/^\[(\d+)]$/);
 
         if (linked) {
-          const t = tags[parseInt(linked)];
+          const t = tags[parseInt(linked[1]) - 1];
           if (t) {
             t.forEach(tag => tagsToRemove.add(tag.name));
           }
         } else if (action.length > 0) {
           tagsToRemove.add(action);
         }
-        return;
       }
       // wildcard deletions
-      if (action.startsWith('?')) {
+      else if (action.startsWith('?')) {
         const match = action.match(/\?(.*)\[(\d+)]/);
         if (match[3]) {
-          const t = tags[parseInt(match[3])];
+          const t = tags[parseInt(match[3]) - 1];
           if (t) {
             t.forEach(tagName => {
               renamesDeletions[tagName.name] = renamesDeletions[tagName.name] || [];
@@ -104,14 +150,13 @@ var YDB_api = YDB_api || {};
             });
           });
         }
-        return;
       }
       // wildcard additions
-      if (action.startsWith('%')) {
+      else if (action.startsWith('%')) {
         const beginning = action.startsWith('%*');
         const match = action.match(/%(\*|)(.*)(\*|)(\[(\d+)]|)/);
         if (match[5]) {
-          const t = tags[parseInt(match[5])];
+          const t = tags[parseInt(match[5]) - 1];
           if (t) {
             t.forEach(tagName => {
               renamesAdditions[tagName.name] = renamesAdditions[tagName.name] || [];
@@ -126,15 +171,15 @@ var YDB_api = YDB_api || {};
             });
           });
         }
-        return;
       }
       // errors
-      if (action.startsWith('^')) {
+      else if (action.startsWith('^')) {
         errors.push(action.substring(1));
-        return;
       }
       // additions
-      tagsToAdd.add(action);
+      else {
+        tagsToAdd.add(action);
+      }
     }
 
     function runRules(tags) {
@@ -153,6 +198,7 @@ var YDB_api = YDB_api || {};
     }
 
     for (let rule in ruleset) {
+      if (rule === rulesetVersionField) continue;
       // first letter - prefix
       const negate = rule.startsWith('^');
       if (negate) {
@@ -170,11 +216,27 @@ var YDB_api = YDB_api || {};
       const dontHaveBanned = foundBannedTags.filter(tags => tags.length > 0).length === 0;
       if ((foundNeeded && dontHaveBanned) ^ negate) {
         const finalTags = foundMatchedTags.map((tags, index) => tags.length > 0 ? tags : foundBannedTags[index]);
-        const actions = ruleset[rule].split('+');
+        const alist = ruleset[rule].replace(/\\\+/g, '[[plus]]');
+        const actions = alist.split('+');
         actions.forEach(action => applyTagRule(action, finalTags, ruleTags));
       }
-      const fTags = dontApply ? tagArray.map(tag => tag.name) : runRules(tagArray.map(tag => tag.name));
-      return {tags: fTags, errors, tagsToRemove, tagsToAdd, renamesDeletions, renamesAdditions};
     }
+    const fTags = dontApply ? tagArray.map(tag => tag.name) : runRules(tagArray.map(tag => tag.name));
+    return {tags: fTags, errors, tagsToRemove, tagsToAdd, renamesDeletions, renamesAdditions};
+  }
+
+  // matched rules will be removed from first ruleset!
+  YDB_api.mergeRulesets = function(old, newer) {
+    const version = 0;
+    if (old[rulesetVersionField] && parseInt(old[rulesetVersionField].split('.')[0]) > version) {
+      throw new Error('Unsupported ruleset version');
+    }
+    if (newer[rulesetVersionField] && parseInt(newer[rulesetVersionField].split('.')[0]) > version) {
+      throw new Error('Unsupported parseInt version');
+    }
+    let temp = Object.assign({}, old);
+    temp = Object.assign(temp, newer);
+    if (old[rulesetVersionField] > newer[rulesetVersionField]) temp[rulesetVersionField] = old[rulesetVersionField];
+    return temp;
   }
 })();
