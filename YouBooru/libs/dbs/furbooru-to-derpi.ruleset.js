488a950c470d06d1ec5e7193d28687e235cdb78a
stsyn <stasyan1902@gmail.com>
2020-09-06 23:40:46 +0500

    Create furbooru-to-derpi.ruleset.js

---
+++ YouBooru/libs/dbs/furbooru-to-derpi.ruleset.js
@@ -0,0 +1,8 @@
+function GetFurbooru2DerpiRuleset() {
+  return [{
+    'not furry': '-',
+    'safe+solo female': '-solo female',
+    'safe+solo male': '-solo male',
+    'safe+solo futa': '-solo futa'
+  }]
+}
