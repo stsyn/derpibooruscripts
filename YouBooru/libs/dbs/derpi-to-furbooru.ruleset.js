e8a388ea2351943d9820b5d9fec0668ed5491ebb
stsyn <stasyan1902@gmail.com>
2020-09-06 23:42:51 +0500

    Update derpi-to-furbooru.ruleset.js

--- YouBooru/libs/dbs/derpi-to-furbooru.ruleset.js
+++ YouBooru/libs/dbs/derpi-to-furbooru.ruleset.js
@@ -4,13 +4,16 @@ function GetDerpi2FurbooruRuleset() {
     '_character': '%* (mlp)+my little pony',
     // adds "not furry" if only "human" as species tag
     '_species[1]+human': 'not furry',
-    // removes "%char%butt"
+    // removes "%character%butt"
     '*butt,!butt,!* butt': '-',
-    'semi-grimdark': '-',
+    // solo * always tagged
     'solo+female': 'solo female',
     'solo+male': 'solo male',
     'solo+futa': 'solo futa',
+    // believe in aliases to cover episode names as well
     'spoiler:*': '-',
+
+    'semi-grimdark': '-',
     'busty *': '-',
   }]
 }
