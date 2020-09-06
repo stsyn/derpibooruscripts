4d40ddb25bd6ae79c458321e58a3844825102e3d
stsyn <stasyan1902@gmail.com>
2020-09-06 10:22:29 +0500

    Update derpi-to-furbooru.ruleset.js

--- YouBooru/libs/dbs/derpi-to-furbooru.ruleset.js
+++ YouBooru/libs/dbs/derpi-to-furbooru.ruleset.js
@@ -1,6 +1,10 @@
 function GetDerpi2FurbooruRuleset() {
   return [{
-    '_character': '%* (mlp)',
+    '_character': '%* (mlp)+my little pony',
     'semi-grimdark': '-',
+    'solo+female': 'solo female',
+    'solo+male': 'solo male',
+    'solo+futa': 'solo futa',
+    'spoiler:*': '-',
   }]
 }
