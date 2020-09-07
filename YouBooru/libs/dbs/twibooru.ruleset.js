9866409439870ab2174c27f84841ceb3854e592f
stsyn <stasyan1902@gmail.com>
2020-09-07 23:21:06 +0500

    Create twibooru.ruleset.js

---
+++ YouBooru/libs/dbs/twibooru.ruleset.js
@@ -0,0 +1,10 @@
+function GetDerpi2TwibooruRuleset() {
+  return [{}];
+}
+
+function GetTwibooru2DerpiRuleset() {
+  return [{
+    'unofficial characters only': '-+oc only',
+    'ponified:*': '?ponified:*',
+  }];
+}
