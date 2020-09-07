e6c64de88708998e0f54e2e006099f1a89303ea7
stsyn <stasyan1902@gmail.com>
2020-09-07 11:04:09 +0500

    Create furbooru.ruleset.js

---
+++ YouBooru/libs/dbs/furbooru.ruleset.js
@@ -0,0 +1,39 @@
+function GetDerpi2FurbooruRuleset() {
+  return [{
+    // adds " (mlp)" to every character tag + "my little pony" tag
+    '_character': '%* (mlp)+my little pony',
+    // adds "not furry" if only "human" as species tag
+    '_species[1]+human': 'not furry',
+    // removes "%character%butt"
+    '*butt,!butt,!* butt': '-',
+    '*betes': '-',
+    'cute*,!cute,!cute *': '-',
+    // solo * always tagged
+    'solo+female': 'solo female',
+    'solo+male': 'solo male',
+    'solo+futa': 'solo futa',
+    // believe in aliases to cover episode names as well
+    'spoiler:*': '-',
+
+    'semi-grimdark': '-',
+    'busty *': '-',
+    'woona': '-',
+  }]
+}
+
+function GetFurbooru2DerpiRuleset() {
+  return [{
+    'not furry': '-',
+    'safe+solo female': '-solo female',
+    'safe+solo male': '-solo male',
+    'safe+solo futa': '-solo futa',
+    'equine': '-',
+    'horse': '-',
+    'mammal': '-',
+    'fictional species': '-',
+    'feral': '-',
+    'friendship is magic': '-',
+    'my little pony': '-',
+  }]
+}
+
