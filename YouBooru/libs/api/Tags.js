06ccbfab0f2b7fc794a2434c89248f46d601102a
stsyn <stasyan1902@gmail.com>
2020-09-23 16:27:58 +0500

    Update Tags.js

--- YouBooru/libs/api/Tags.js
+++ YouBooru/libs/api/Tags.js
@@ -18,6 +18,10 @@ var YDB_api = YDB_api || {};
     return 'unknown';
   }
 
+  function __unique(item, index, array) {
+    return index === array.indexOf(item);
+  }
+
   function getCommonTagFields(tag) {
     return {
       category: tag.category,
@@ -29,36 +33,18 @@ var YDB_api = YDB_api || {};
       namespace: tag.namespace,
       short_description: tag.short_description,
       slug: tag.slug,
-      spoiler_image_uri: tag.spoiler_image_uri
+      spoiler_image_uri: tag.spoiler_image_uri,
+      synonyms: []
     }
   }
 
-  // todo: paginating should be redone
-  // add optional way to fetch additional data on BoR
-  YDB_api.fetchManyTagsByName = async function(tagNames, page = 0) {
+  async function fetchTagPage(tagNames) {
     const enviroment = __getEnviroment();
-
-    const maxTagsPerPage = 25;
-    let pages = 1;
-    // philomena requires pagination
-    if (tagNames.length > maxTagsPerPage && enviroment === 'philomena' && page === 0) {
-      const result = {};
-      for (let i = 0; i < Math.ceil(tagNames.length / maxTagsPerPage); i++) {
-        const fetched = await YDB_api.fetchManyTagsByName(tagNames, i + 1);
-        for (let tag in fetched) {
-          if (fetched[tag]) {
-            result[tag] = fetched[tag];
-          }
-        }
-      }
-      return result;
-    }
 
     let url;
     if (enviroment === 'philomena') {
       // for philomena use tag search ability
       url = '/api/v1/json/search/tags?q=name:' + tagNames.join("+OR+name:");
-      if (page) url += '&page=' + page;
     } else if (enviroment === 'bor') {
       // for bor use tag batch fetch
       url = '/api/v2/tags/fetch_many.json?name[]=' + tagNames.join("&name[]=");
@@ -76,6 +62,13 @@ var YDB_api = YDB_api || {};
     if (enviroment === 'philomena') {
       content.tags = content.tags.map(tag => {
         let newTag = getCommonTagFields(tag);
+        newTag.dnp_entries = tag.dnp_entries.map(dnp => {
+          return {
+            dnp_type: dnp.dnp_type || 'Unknown',
+            conditions: dnp.conditions || 'Unknown',
+            reason: dnp.reason || 'byte[], fix philomena plz',
+          }
+        });
         newTag.aliases = tag.aliases.map(__slugToTag);
         newTag.aliased_tag = __slugToTag(tag.aliased_tag || '');
         newTag.implied_tags = tag.implied_tags.map(__slugToTag);
@@ -86,6 +79,7 @@ var YDB_api = YDB_api || {};
     if (enviroment === 'bor') {
       content.tags = content.tags.map(tag => {
         let newTag = getCommonTagFields(tag);
+        newTag.dnp_entries = [];
         newTag.aliases = [];
         newTag.aliased_tag = tag.aliased_to;
         newTag.implied_tags = tag.implied_tags;
@@ -96,7 +90,7 @@ var YDB_api = YDB_api || {};
     const result = {};
 
     tagNames.forEach(name => {
-      result[name] = content.tags.find(tag => tag.name === name);
+      result[name] = content.tags.find(tag => tag.name === name) || {name, empty: true};
     });
 
     const secondPass = content.tags.filter(tag => tag.aliased_tag);
@@ -105,6 +99,59 @@ var YDB_api = YDB_api || {};
       secondPass.forEach(tag => {
         result[tag.name] = secondResult[tag.aliased_tag];
       });
+    }
+    return result;
+  }
+
+  YDB_api.fetchTagByName = async function(tagName) {
+    return await YDB_api.fetchTagBySlug(__tagToSlug(tagName));
+  }
+
+  YDB_api.fetchTagBySlug = async function(tagSlug) {
+    // todo
+  }
+
+  // todo: add optional way to fetch additional dnp data on Philomena
+  YDB_api.fetchManyTagsByName = async function(tagNames, options = {}) {
+    if (options.resolveSynonims === undefined) options.resolveSynonims = false;
+    if (options.alwaysLoadAliases === undefined) options.alwaysLoadAliases = false;
+    if (options.alwaysLoadDnp === undefined) options.alwaysLoadDnp = false;
+
+    const enviroment = __getEnviroment();
+
+    const maxTagsPerPage = 25;
+    const result = {};
+    const parts = tagNames.reduce((acc, tag, index) => {
+      const page = Math.ceil(index / maxTagsPerPage);
+      if (!acc[page]) acc[page] = [];
+      acc[page].push(tag);
+      return acc;
+    }, []);
+    for (let part of parts) {
+      const fetched = await fetchTagPage(tagNames);
+      part.forEach(tag => {
+        result[tag] = fetched[tag] || {name: tag, empty: true};
+      });
+    }
+
+    if (options.resolveSynonims) {
+      const tagsToLook = Object.values(result).filter(tag => !tag.empty).map(tag => tag.implied_tags).flat().filter(__unique).filter(tag => !result[tag]);
+      const details = await YDB_api.fetchManyTagsByName(tagsToLook);
+      Object.values(details).forEach(tag => {
+        tag.implied_tags.forEach(implied => {
+          const impliedTag = result[implied];
+          if (impliedTag && impliedTag.implied_tags.includes(tag.name)) {
+            impliedTag.synonyms.push(tag.name);
+          }
+        });
+      });
+    }
+    if (options.alwaysLoadAliases && enviroment === 'bor') {
+      const tagsToLook = Object.values(result).filter(tag => !tag.empty).map(tag => tag.name);
+      const details = [];
+      for (let tagName of tagsToLook) {
+        details.push(await YDB_api.fetchTagByName(tagName));
+      }
     }
     return result;
   }
