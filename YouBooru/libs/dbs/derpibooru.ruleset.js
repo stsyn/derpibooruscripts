fc773356b20674abd2c7bbbdb9a6b2debab48e08
stsyn <stasyan1902@gmail.com>
2020-09-23 14:02:36 +0500

    Update derpibooru.ruleset.js

--- YouBooru/libs/dbs/derpibooru.ruleset.js
+++ YouBooru/libs/dbs/derpibooru.ruleset.js
@@ -1,11 +1,13 @@
 function GetDerpibooruRuleset() {
   return [{
+    '[{[version]}]': '0.0',
     '*[<3]': '^[E]At least 3 tags required!',
     '^_rating': '^[E]You forgot rating tags!',
     'safe+_rating[>1]': '^[E]You cannot use "safe" with any other rating tags!',
     'suggestive,questionable,explicit[[>1]]': '^[E]You cannot use more than one sexual ratings tag!',
     'grimdark+semi-grimdark': '^[E]You cannot use more than one grimdark ratings tag!'
   }, {
+    '[{[version]}]': '0.0',
     '^artist:*,screencap,artist needed,anonymous artist': '^[E]No origin provided!',
     '^_character,_oc,no pony': '^[E]No character tag!',
     'safe+solo female,solo male, solo futa': 'solo+^[E]%tag:solo *% should be used only in sexual context. Use just %tag:solo% and correct gender tag!',
