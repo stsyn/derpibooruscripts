99e0716cf03f70c82aa36d2f02705571a368f97d
stsyn <stasyan1902@gmail.com>
2020-09-23 18:17:49 +0500

    Update derpibooru.ruleset.js

--- YouBooru/libs/dbs/derpibooru.ruleset.js
+++ YouBooru/libs/dbs/derpibooru.ruleset.js
@@ -12,7 +12,7 @@ function GetDerpibooruRuleset() {
     '^_character,_oc,no pony': '^[E]No character tag!',
     'safe+solo female,solo male, solo futa': 'solo+^[E]%tag:solo *% should be used only in sexual context. Use just %tag:solo% and correct gender tag!',
     'solo+male,female,futa[[>1]]': '^[E]%tag:female%, %tag:futa%, %tag:male% with %tag:solo%.',
-    'solo+_character,_oc[[>1]]+-fusion': '^[E]Multiple characters while %tag:solo% tagged.',
+    'solo+_character,oc:*[[>1]]+-fusion': '^[E]Multiple characters while %tag:solo% tagged.',
 
     '_oc': 'oc',
     '_oc+-_character': 'oc only',
