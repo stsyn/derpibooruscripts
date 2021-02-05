c9c2fcd6c14b459aeb122a2e628ee0791b4409c5
stsyn <stasyan1902@gmail.com>
2021-02-05 19:41:49 +0500

    Create themeData.js

---
+++ YouBooru/libs/themeData.js
@@ -0,0 +1,76 @@
+const ponymarks = {
+    bgs:{
+      aj:'https://camo.derpicdn.net/9ec1a92c1d98be04d750f719c5285e14eb0af3e7?url=https%3A%2F%2Fi.imgur.com%2FiOy09ti.png',
+      dh:'https://camo.derpicdn.net/497efe3f86ad217bd0999dd1c904e4cb9c9f897c?url=https%3A%2F%2Fi.imgur.com%2FOdsgUat.png',
+      fs:'https://camo.derpicdn.net/45c6d0533de22ccbbbccb77c44b9f0c6229c67ab?url=https%3A%2F%2Fi.imgur.com%2Fc3Bo4i8.png',
+      luna:'https://camo.derpicdn.net/4d4c82f4c20c3dae40587b1836decec85ae506a9?url=https%3A%2F%2Fi.imgur.com%2FpmcGux8.png',
+      lyra:'https://camo.derpicdn.net/a80100c0596a68ff8881c80f3b8a168f98b58825?url=https%3A%2F%2Fi.imgur.com%2FMijgqfu.png',
+      pcd:'https://camo.derpicdn.net/ef8cf240f883a12285720569f2f1a02c75104942?url=https%3A%2F%2Fi.imgur.com%2F5yreNg4.png',
+      pp:'https://camo.derpicdn.net/ea8d8c75578126a1102161eb29fed0c95a2232fd?url=https%3A%2F%2Fi.imgur.com%2FNDUyAIQ.png',
+      rd:'https://camo.derpicdn.net/f9851bd532e626c21f28c740153bf57724596a1d?url=https%3A%2F%2Fi.imgur.com%2FC70ULye.png',
+      ry:'https://camo.derpicdn.net/681bbc4eb5c3d1cfb52ed0314394e13759ca1201?url=https%3A%2F%2Fi.imgur.com%2FFE1oL6K.png',
+      sg:'https://camo.derpicdn.net/02c657a4380895b264b92bddfcde31c71cf69732?url=https%3A%2F%2Fi.imgur.com%2F7P3D8xl.png',
+      tia:'https://camo.derpicdn.net/59f353243662b65b8b5db8553d2523a86fbd8ecd?url=https%3A%2F%2Fi.imgur.com%2F6VxwK0n.png',
+      ts:'https://camo.derpicdn.net/361a9e94f8f37e4d8175a1ceb361134e8eb00b73?url=https%3A%2F%2Fi.imgur.com%2FURcKMiB.png',
+      tx:'https://camo.derpicdn.net/8c9b2c6232506c634e588162402fb5020e783c39?url=https%3A%2F%2Fi.imgur.com%2FRQHaAEG.png'
+    },
+    defaults:{
+      'content-fanmade': 'pp',
+      species:'none',
+      oc: 'ts',
+      error: 'tia',
+      spoiler: 'aj',
+      'content-official': 'fs',
+      '': 'lyra',
+      character: 'rd',
+      rating: 'luna',
+      origin: 'ry',
+      'body-type': 'dh'
+    },
+    fix_brightness: {
+      light:{},
+      dark:{
+        spoiler: 150,
+        'content-official':160
+      },
+      red:{}
+    },
+    filter:{
+      dark:{
+        species: 45,
+        'content-fanmade': 320,
+        oc: 300,
+        error: 0,
+        spoiler: 60,
+        'content-official': 70,
+        '': 120,
+        character:183,
+        rating:210,
+        origin:265
+      },
+      light:{
+        species: 185,
+        'content-fanmade': 170,
+        oc: 150,
+        error: 180,
+        spoiler: 190,
+        'content-official':255,
+        '': 270,
+        character:340,
+        rating:60,
+        origin:70
+      },
+      red:{
+        species: 60,
+        'content-fanmade': 320,
+        oc: 300,
+        error: 0,
+        spoiler: 60,
+        'content-official': 70,
+        '': 120,
+        character:180,
+        rating:210,
+        origin:280
+      }
+    }
+  };
