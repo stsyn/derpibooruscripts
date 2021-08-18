79d9d202995d0229edc2edc7838c71c7c87278c4
stsyn <stasyan1902@gmail.com>
2021-08-18 16:08:28 +0300

    Update Inputs.js

--- YouBooru/libs/ui/Inputs.js
+++ YouBooru/libs/ui/Inputs.js
@@ -6,7 +6,7 @@ YDB_api.UI.input = props => {
   const id = otherProps.id || `${otherProps.name}-${Math.floor(Math.random() * 10000)}`;
 
   return ['.field', [
-    label ? ['label', { for: id }, label] : null,
+    label ? ['label', { for: id }, label + ' '] : null,
     ['input.input', { ...otherProps, className: fullWidth ? 'input--wide' : '' , id }],
     fieldLabel ? ['div', [
       ['i', fieldLabel],
@@ -19,7 +19,7 @@ YDB_api.UI.textarea = props => {
   const id = otherProps.id || `${otherProps.name}-${Math.floor(Math.random() * 10000)}`;
 
   return ['.field', [
-    label ? ['label', { for: id }, label] : null,
+    label ? ['label', { for: id }, label + ' '] : null,
     ['textarea.input', { ...otherProps, className: fullWidth ? 'input--wide' : '' , id }],
     fieldLabel ? ['div', [
       ['i', fieldLabel],
