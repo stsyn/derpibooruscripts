46137d39cffad53602dd3356beee94ce6c35ed4c
stsyn <stasyan1902@gmail.com>
2021-09-09 14:28:14 +0300

    Update Inputs.js

--- YouBooru/libs/ui/Inputs.js
+++ YouBooru/libs/ui/Inputs.js
@@ -26,3 +26,33 @@ YDB_api.UI.textarea = props => {
     ]] : null,
   ]]
 }
+
+YDB_api.UI.checkbox = props => {
+  const { label, fieldLabel, value, ...otherProps } = props;
+  const id = otherProps.id || `${otherProps.name}-${Math.floor(Math.random() * 10000)}`;
+
+  return ['.field', [
+    label ? ['label', { for: id }, label + ' '] : null,
+    ['input', { ...otherProps, type: 'checkbox', checked: value , id }],
+    fieldLabel ? ['div', [
+      ['i', fieldLabel],
+    ]] : null,
+  ]]
+}
+
+YDB_api.UI.fuckIe = () => (
+  ['input', { type: 'hidden', id: 'fuck_ie', name: 'fuck_ie[fuck_ie]', value: 'fuck_ie'}]
+)
+
+YDB_api.UI.button = (props, content) => {
+  const { bold, state, className, ...otherProps } = props;
+
+  return [
+    'button.button',
+    {
+      ...otherProps,
+      className: (className ? className : '') + ' ' + (bold ? 'button--bold ': '') + (state ? 'button--state-' + state : ''),
+    },
+    content,
+  ]
+}
