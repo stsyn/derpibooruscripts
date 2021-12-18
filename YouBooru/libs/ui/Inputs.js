cbeb5852114f3c9ba663b0393d295c5a0aeda05f
stsyn <stasyan1902@gmail.com>
2021-12-18 11:54:40 +0300

    Update Inputs.js

--- YouBooru/libs/ui/Inputs.js
+++ YouBooru/libs/ui/Inputs.js
@@ -2,12 +2,16 @@ var YDB_api = YDB_api || {};
 YDB_api.UI = YDB_api.UI || {};
 
 YDB_api.UI.input = props => {
-  const { label, fieldLabel, fullWidth, ...otherProps } = props;
+  const { label, fieldLabel, fullWidth, className, onchange, inline, _redraw, ...otherProps } = props;
   const id = otherProps.id || `${otherProps.name}-${Math.floor(Math.random() * 10000)}`;
 
-  return ['.field', [
+  if (inline) {
+    return ['input.input', { ...otherProps, oninput: onchange, onchange, _redraw, className: (fullWidth ? 'input--wide' : '') + (className || '') , id }];
+  }
+
+  return ['.field', { _redraw }, [
     label ? ['label', { for: id }, label + ' '] : null,
-    ['input.input', { ...otherProps, className: fullWidth ? 'input--wide' : '' , id }],
+    ['input.input', { ...otherProps, oninput: onchange, onchange, className: (fullWidth ? 'input--wide' : '') + (className || '') , id }],
     fieldLabel ? ['div', [
       ['i', fieldLabel],
     ]] : null,
@@ -15,12 +19,33 @@ YDB_api.UI.input = props => {
 }
 
 YDB_api.UI.textarea = props => {
-  const { label, fieldLabel, fullWidth, ...otherProps } = props;
+  const { label, fieldLabel, fullWidth, className, onchange, _redraw, ...otherProps } = props;
   const id = otherProps.id || `${otherProps.name}-${Math.floor(Math.random() * 10000)}`;
 
-  return ['.field', [
+  return ['.field', { _redraw }, [
     label ? ['label', { for: id }, label + ' '] : null,
-    ['textarea.input', { ...otherProps, className: fullWidth ? 'input--wide' : '' , id }],
+    ['textarea.input', { ...otherProps, oninput: onchange, onchange, className: (fullWidth ? 'input--wide ' : '') + (className || ''), id }],
+    fieldLabel ? ['div', [
+      ['i', fieldLabel],
+    ]] : null,
+  ]]
+}
+
+YDB_api.UI.select = props => {
+  const { label, fieldLabel, fullWidth, className, _redraw, inline, options, ...otherProps } = props;
+  const id = otherProps.id || `${otherProps.name}-${Math.floor(Math.random() * 10000)}`;
+
+  if (inline) {
+    return ['select.input', { ...otherProps, _redraw, className: (fullWidth ? 'input--wide' : '') + (className || '') , id }, options.map(option => (
+      ['option', { value: option.value, checked: option.checked }, [option.name]]
+    ))];
+  }
+
+  return ['.field', { _redraw }, [
+    label ? ['label', { for: id }, label + ' '] : null,
+    ['select.input', { ...otherProps, className: (fullWidth ? 'input--wide' : '') + (className || '') , id }, options.map(option => (
+      ['option', { value: option.value, checked: option.checked }, [option.name]]
+    ))],
     fieldLabel ? ['div', [
       ['i', fieldLabel],
     ]] : null,
@@ -28,10 +53,10 @@ YDB_api.UI.textarea = props => {
 }
 
 YDB_api.UI.checkbox = props => {
-  const { label, fieldLabel, value, ...otherProps } = props;
+  const { label, fieldLabel, value, _redraw, ...otherProps } = props;
   const id = otherProps.id || `${otherProps.name}-${Math.floor(Math.random() * 10000)}`;
 
-  return ['.field', [
+  return ['.field', { _redraw }, [
     label ? ['label', { for: id }, label + ' '] : null,
     ['input', { ...otherProps, type: 'checkbox', checked: value , id }],
     fieldLabel ? ['div', [
@@ -51,7 +76,7 @@ YDB_api.UI.button = (props, content) => {
     'button.button',
     {
       ...otherProps,
-      className: (className ? className : '') + ' ' + (bold ? 'button--bold ': '') + (state ? 'button--state-' + state : ''),
+      className: (className || '') + ' ' + (bold ? 'button--bold ': '') + (state ? 'button--state-' + state : ''),
     },
     content,
   ]
